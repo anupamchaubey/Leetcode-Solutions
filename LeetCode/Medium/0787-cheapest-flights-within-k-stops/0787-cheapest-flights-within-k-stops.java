eec769a5a188de8ccb60f25866afb1b176dfd899
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-02-20 18:17:16 +0530

    0787-cheapest-flights-within-k-stops .java 6 ms (86.78%)

--- LeetCode/Medium/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ LeetCode/Medium/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -2,44 +2,39 @@ class Solution {
     class Pair{
         int node;
         int price;
-        Pair(int n, int price){
-            this.node=n;
+        public Pair(int node, int price){
+            this.node=node;
             this.price=price;
         }
     }
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<List<Pair>> adj= new ArrayList<>();
+        List<List<Pair>> adj=new ArrayList<>();
         for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-        int[] dist=new int[n];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        for(int[] f:flights){
-            int s=f[0];
-            int d=f[1];
-            int p=f[2];
-            adj.get(s).add(new Pair(d, p));
+        for(int i=0;i<flights.length;i++){
+            int a=flights[i][0];
+            int b=flights[i][1];
+            int w=flights[i][2];
+            adj.get(a).add(new Pair(b,w));
         }
-
-        Queue<int[]> pq= new LinkedList<>();
-        pq.offer(new int[]{src, 0, 0});
+        int[] dist= new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
         dist[src]=0;
-
-        while(!pq.isEmpty()){
-            int[] curr=pq.poll();
-            int s=curr[0];
-            int price=curr[1];
-            int stops=curr[2];
-            if(stops>k)continue;
-
-            for(Pair p: adj.get(s)){
-                int ns=p.node;
-                int nprice=price+p.price;
-                int nstops=stops+1;
-
-                if(dist[ns]>nprice ){
-                    dist[ns]=nprice;
-                    pq.offer(new int[]{ns, dist[ns], nstops});
+        Queue<int[]> q= new LinkedList<>();
+        q.offer(new int[]{src, 0, 0});
+        while(!q.isEmpty()){
+            int[] curr=q.poll();
+            int node=curr[0];
+            int d=curr[1];
+            int st=curr[2];
+            if(st>k)continue;
+            for(Pair p: adj.get(node)){
+                int nr=p.node;
+                int nd=p.price+d;
+                if(dist[nr]>nd){
+                    dist[nr]=nd;
+                    q.offer(new int[]{nr, nd, st+1});
                 }
             }
         }
