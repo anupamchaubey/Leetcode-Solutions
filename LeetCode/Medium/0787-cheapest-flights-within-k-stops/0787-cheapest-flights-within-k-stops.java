934bfd71ae6a3a1ee6295dda5b08b8c87e0fb910
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-02-22 00:20:20 +0530

    0787-cheapest-flights-within-k-stops .java 6 ms (86.79%)

--- LeetCode/Medium/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ LeetCode/Medium/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,19 +1,24 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        // use bellman-ford algo
-        long[] dist=new long[n];
-        Arrays.fill(dist, Long.MAX_VALUE);
-        dist[src]=0;
+        int[] price= new int[n];
+        Arrays.fill(price, Integer.MAX_VALUE);
+        price[src]=0;
+
+        int INF=Integer.MAX_VALUE;
+
         for(int i=0;i<=k;i++){
-            long[] temp=dist.clone();
+            int[] temp=price.clone();
             for(int[] f: flights){
-                int u=f[0], v=f[1], w=f[2];
-                if(dist[u]!=Long.MAX_VALUE && dist[u]+w<temp[v]){
-                    temp[v]=dist[u]+w;
+                int u=f[0];
+                int v=f[1];
+                int c=f[2];
+                if(price[u]!=INF){
+                    temp[v]=Math.min(temp[v], price[u]+c);
                 }
             }
-            dist=temp;
+            price=temp;
         }
-        return dist[dst]==Long.MAX_VALUE? -1: (int)dist[dst];
+        return price[dst]==INF? -1: price[dst];
+
     }
 }
