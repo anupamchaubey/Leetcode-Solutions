ce5923430127c0926f59f36b8ea615a755f6aa42
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-02-21 22:29:31 +0530

    0133-clone-graph .java 23 ms (98.93%)

---
+++ LeetCode/Medium/0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    Map<Node, Node> visited= new HashMap<>();
+    public Node cloneGraph(Node node) {
+        if(node==null)return null;
+        Node newNode=dfs(node);
+        return newNode;
+
+    }
+    Node dfs(Node node){
+        if(visited.containsKey(node)){
+            return visited.get(node);
+        }
+        Node newNode=new Node(node.val);
+        visited.put(node, newNode);
+        
+        for(Node i:node.neighbors){
+            newNode.neighbors.add(dfs(i));
+        }
+        return newNode;
+        
+    }
+}
