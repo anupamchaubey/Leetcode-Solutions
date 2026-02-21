93083ceb4752d4cda3269ef6a3373842bda56222
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-02-22 00:38:22 +0530

    0787-cheapest-flights-within-k-stops .java 6 ms (86.79%)

--- LeetCode/Medium/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ LeetCode/Medium/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,24 +1,48 @@
 class Solution {
+    class Pair {
+        int node;
+        int price;
+
+        public Pair(int node, int price) {
+            this.node = node;
+            this.price = price;
+        }
+    }
+
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int[] price= new int[n];
-        Arrays.fill(price, Integer.MAX_VALUE);
-        price[src]=0;
-
-        int INF=Integer.MAX_VALUE;
-
-        for(int i=0;i<=k;i++){
-            int[] temp=price.clone();
-            for(int[] f: flights){
-                int u=f[0];
-                int v=f[1];
-                int c=f[2];
-                if(price[u]!=INF){
-                    temp[v]=Math.min(temp[v], price[u]+c);
+        List<List<Pair>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int i = 0; i < flights.length; i++) {
+            int a = flights[i][0];
+            int b = flights[i][1];
+            int w = flights[i][2];
+            adj.get(a).add(new Pair(b, w));
+        }
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] { src, 0, 0 });
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            int node = curr[0];
+            int d = curr[1];
+            int st = curr[2];
+            if (st > k)
+                continue;
+            for (Pair p : adj.get(node)) {
+                int nr = p.node;
+                int nd = p.price + d;
+                if (dist[nr] > nd) {
+                    dist[nr] = nd;
+                    q.offer(new int[] { nr, nd, st + 1 });
                 }
             }
-            price=temp;
         }
-        return price[dst]==INF? -1: price[dst];
-
+        if (dist[dst] == Integer.MAX_VALUE)
+            return -1;
+        return dist[dst];
     }
 }
