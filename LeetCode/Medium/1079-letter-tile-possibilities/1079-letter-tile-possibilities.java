59b1f8c81bbb9eb78ab4e2e670db10ed59b4c6c7
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-01 14:54:09 +0530

    1079-letter-tile-possibilities .java

--- LeetCode/Medium/1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
+++ LeetCode/Medium/1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
@@ -3,19 +3,17 @@ class Solution {
 
     public int numTilePossibilities(String tiles) {
         boolean[] visited = new boolean[tiles.length()];
-        re(tiles, 0, "", visited);
-        return hs.size() ;
+        re(tiles, "", visited);
+        return hs.size();
     }
 
-  
+    void re(String tiles, String s, boolean[] visited) {
 
-    void re(String tiles, int idx, String s, boolean[] visited) {
-        
         for (int i = 0; i < tiles.length(); i++) {
             if (!visited[i]) {
                 visited[i] = true;
                 hs.add(s + tiles.charAt(i));
-                re(tiles, idx + 1, s + tiles.charAt(i), visited);
+                re(tiles, s + tiles.charAt(i), visited);
                 visited[i] = false;
             }
         }
