65e7b65dde10190e4183af753a731dba796d4a4a
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-02 10:12:38 +0530

    0650-2-keys-keyboard .java

--- LeetCode/Medium/0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ LeetCode/Medium/0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -3,14 +3,12 @@ class Solution {
         if(n==1)return 0;
         return rec(n, 1, 1)+1;
     }
-    int rec(int n, int cnt, int paste){
-        if(cnt==n)return 0;
-        if(cnt>n)return Integer.MAX_VALUE/3;
+    int rec(int n, int letters, int copied){
+        if(letters==n)return 0;
+        if(letters>n)return Integer.MAX_VALUE/3;
         int min=Integer.MAX_VALUE;
-        //paste
-        min=Math.min(min, 1+rec(n, cnt+paste, paste));
-        //copy all + paste
-        min=Math.min(min, 2+rec(n, cnt+cnt, cnt));
+        min=Math.min(min, 1+rec(n, letters+copied, copied));
+        min=Math.min(min, 2+rec(n, letters+letters, letters));
         return min;
     }
 }
