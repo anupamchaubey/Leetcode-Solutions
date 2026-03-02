aef1e798c9e0164e821b4b336cdefa277e51088f
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-02 10:05:15 +0530

    0650-2-keys-keyboard .java

--- LeetCode/Medium/0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ LeetCode/Medium/0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minSteps(int n) {
         if(n==1)return 0;
-        return rec(n, 1, 1)+1 ;
+        return rec(n, 1, 1)+1;
     }
     int rec(int n, int cnt, int paste){
         if(cnt==n)return 0;
