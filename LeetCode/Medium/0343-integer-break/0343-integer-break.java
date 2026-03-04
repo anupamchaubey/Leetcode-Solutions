7bafe5966a6dea7431d9f204cd8d8d27a5d3f72b
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-04 13:59:20 +0530

    0343-integer-break .java

--- LeetCode/Medium/0343-integer-break/0343-integer-break.java
+++ LeetCode/Medium/0343-integer-break/0343-integer-break.java
@@ -1,26 +1,18 @@
 class Solution {
-/*
-
- n=3∗k+r where r=0,2,4
-A pow solution is possible.
-
-*/
-// maximum jitna 3 hoga utni bdi value
-
+    // DP solution
     public int integerBreak(int n) {
-        if (n == 3)
-            return 2;
-        if (n == 2)
-            return 1;
-        int ans = 1;
-        while (n > 0) {
-            if (n == 2 || n == 4) {
-                ans *= n;
-                break;
-            }
-            ans *= 3;
-            n -= 3;
+        return rec(n);
+    }
+
+    int rec(int n) {
+        if (n == 1)
+            return 0;
+        
+        int max = 0;
+        for (int i = 1; i < n; i++) {
+            max = Math.max(max, i * Math.max(n-i, rec(n - i)));
         }
-        return ans;
+        return max;
+
     }
 }
