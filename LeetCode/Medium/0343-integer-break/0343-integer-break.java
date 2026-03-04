441f7cc65ca3352a2b15bfa64a168af539b1fe85
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-04 14:04:56 +0530

    0343-integer-break .java

--- LeetCode/Medium/0343-integer-break/0343-integer-break.java
+++ LeetCode/Medium/0343-integer-break/0343-integer-break.java
@@ -1,20 +1,28 @@
 class Solution {
     // DP solution
     Integer[] memo;
+
     public int integerBreak(int n) {
-        memo=new Integer[n+1];
-        return rec(n);
+        int[] dp= new int[n+1];
+        dp[1]=0;
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<i;j++){
+                dp[i]=Math.max(dp[i], Math.max(j*(i-j), j*dp[i-j]));
+            }
+        }
+        return dp[n];
     }
 
     int rec(int n) {
         if (n == 1)
             return 0;
-        if(memo[n]!=null)return memo[n];
+        if (memo[n] != null)
+            return memo[n];
         int max = 0;
         for (int i = 1; i < n; i++) {
-            max = Math.max(max, i * Math.max(n-i, rec(n - i)));
+            max = Math.max(max, i * Math.max(n - i, rec(n - i)));
         }
-        return memo[n]= max;
+        return memo[n] = max;
 
     }
 }
