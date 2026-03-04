1e3f2e742f5704d478cb67bdda3586a07f626c42
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-04 14:18:14 +0530

    0279-perfect-squares .java

--- LeetCode/Medium/0279-perfect-squares/0279-perfect-squares.java
+++ LeetCode/Medium/0279-perfect-squares/0279-perfect-squares.java
@@ -1,7 +1,14 @@
 class Solution {
     public int numSquares(int n) {
-        Integer[] memo = new Integer[n + 1];
-        return rec(n, memo);
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], 1 + dp[i - j * j]);
+            }
+        }
+        return dp[n];
     }
 
     int rec(int n, Integer[] memo) {
