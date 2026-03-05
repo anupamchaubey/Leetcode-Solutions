74f60949e3ad2da964a14669365d9c84c2df430b
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-05 13:28:22 +0530

    1758-minimum-changes-to-make-alternating-binary-string .java

---
+++ LeetCode/Easy/1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minOperations(String s) {
+        int c1 = 0;
+        int c2 = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 == 0 && s.charAt(i) != '1')
+                c1++;
+            else if (i % 2 == 1 && s.charAt(i) != '0')
+                c1++;
+            if (i % 2 == 1 && s.charAt(i) != '1')
+                c2++;
+            else if (i % 2 == 0 && s.charAt(i) != '0')
+                c2++;
+        }
+        return Math.min(c1, c2);
+    }
+}
