a01ea932930d15d1eeca78ee9c5cd52d4900aeaa
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-08 15:15:34 +0530

    3864-minimum-cost-to-partition-a-binary-string .java

---
+++ LeetCode/Hard/3864-minimum-cost-to-partition-a-binary-string/3864-minimum-cost-to-partition-a-binary-string.java
@@ -0,0 +1,46 @@
+class Solution {
+
+    public long minCost(String s, int enc, int flat) {
+        return rec(s, 0, s.length() - 1, enc, flat);
+    }
+
+    long rec(String s, int st, int en, int enc, int flat) {
+
+        int len = en - st + 1;
+        int count = sen(s, st, en);
+
+        long store;
+        if (count == 0)
+            store = flat;
+        else
+            store = (long) len * count * enc;
+
+        if (st == en)
+            return store;
+
+        if (len % 2 == 0) {
+
+            int mid = (st + en) / 2;
+
+            long left = rec(s, st, mid, enc, flat);
+            long right = rec(s, mid + 1, en, enc, flat);
+
+            long split = left + right;
+
+            return Math.min(store, split);
+        }
+
+        return store;
+    }
+
+    int sen(String s, int i, int j) {
+        int c = 0;
+
+        for (int k = i; k <= j; k++) {
+            if (s.charAt(k) == '1')
+                c++;
+        }
+
+        return c;
+    }
+}
