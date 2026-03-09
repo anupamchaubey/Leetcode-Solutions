a163d2d8cee22da8315264881c21fcefa2df9c21
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-09 19:13:47 +0530

    0105-construct-binary-tree-from-preorder-and-inorder-traversal .java

--- LeetCode/Medium/0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ LeetCode/Medium/0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,20 +1,23 @@
 class Solution {
     int preorderIndex;
-    HashMap<Integer, Integer> hm=new HashMap<>();
+    HashMap<Integer, Integer> hm = new HashMap<>();
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        preorderIndex=0;
-        for(int i=0;i<inorder.length;i++){
+        preorderIndex = 0;
+        for (int i = 0; i < inorder.length; i++) {
             hm.put(inorder[i], i);
         }
-        return rec(preorder, 0, inorder.length-1);
+        return rec(preorder, 0, inorder.length - 1);
     }
-    TreeNode rec(int[] preorder, int l, int r){
-        if(l>r)return null;
-        int rootVal=preorder[preorderIndex++];
-        TreeNode root=new TreeNode(rootVal);
-        int idx=hm.get(rootVal);
-        root.left=rec(preorder, l, idx-1);
-        root.right=rec(preorder, idx+1, r);
+
+    TreeNode rec(int[] preorder, int l, int r) {
+        if (l > r)
+            return null;
+        int rootVal = preorder[preorderIndex++];
+        TreeNode root = new TreeNode(rootVal);
+        int idx = hm.get(rootVal);
+        root.left = rec(preorder, l, idx - 1);
+        root.right = rec(preorder, idx + 1, r);
         return root;
     }
 }
