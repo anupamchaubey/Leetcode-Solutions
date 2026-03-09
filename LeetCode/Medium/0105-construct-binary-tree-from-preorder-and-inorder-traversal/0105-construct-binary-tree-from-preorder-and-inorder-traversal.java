c9eac497f8b622bc539de6e7b90a18601cac2263
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-09 19:13:35 +0530

    0105-construct-binary-tree-from-preorder-and-inorder-traversal .java

--- LeetCode/Medium/0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ LeetCode/Medium/0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,36 +1,20 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    int idx=0;
+    int preorderIndex;
+    HashMap<Integer, Integer> hm=new HashMap<>();
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return helper(preorder, inorder, 0, preorder.length-1);
-    }
-    TreeNode helper(int[] preorder, int[] inorder, int st, int en){
-        if(st>en)return null;
-        int rootval=preorder[idx++];
-        int index=-1;
-        for(int i=st;i<=en;i++){
-            if(inorder[i]==rootval){
-                index=i;
-                break;
-            }
+        preorderIndex=0;
+        for(int i=0;i<inorder.length;i++){
+            hm.put(inorder[i], i);
         }
-        TreeNode root=new TreeNode(rootval);
-        root.left=helper(preorder, inorder, st, index-1);
-        root.right=helper(preorder, inorder, index+1, en);
+        return rec(preorder, 0, inorder.length-1);
+    }
+    TreeNode rec(int[] preorder, int l, int r){
+        if(l>r)return null;
+        int rootVal=preorder[preorderIndex++];
+        TreeNode root=new TreeNode(rootVal);
+        int idx=hm.get(rootVal);
+        root.left=rec(preorder, l, idx-1);
+        root.right=rec(preorder, idx+1, r);
         return root;
     }
 }
