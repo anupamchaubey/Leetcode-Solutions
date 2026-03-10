809abb8c9116d3c37cd58ac7b6a3c2462ed2d0ee
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-10 21:24:06 +0530

    0098-validate-binary-search-tree .java

--- LeetCode/Medium/0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ LeetCode/Medium/0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,28 +1,28 @@
 class Solution {
-    // RANGE METHOD 
-    // public boolean isValidBST(TreeNode root) {
-    //     return validate(root,Long.MIN_VALUE, Long.MAX_VALUE);
-    // }
-    // boolean validate(TreeNode root, long min, long max){
-    //     if(root==null)return true;
-    //     if(root.val<= min || root.val>=max)return false;
-    //     return validate(root.left, min, root.val)&& validate(root.right, root.val, max);
-    // }
-
-    List<Integer> ls= new ArrayList<>();
-    public boolean isValidBST(TreeNode root){
-        inorder(root);
-        for(int i=0;i<ls.size()-1;i++){
-            if(ls.get(i)>=ls.get(i+1))return false;
-        }
-        return true;
+    RANGE METHOD 
+    public boolean isValidBST(TreeNode root) {
+        return validate(root,Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    boolean validate(TreeNode root, long min, long max){
+        if(root==null)return true;
+        if(root.val<= min || root.val>=max)return false;
+        return validate(root.left, min, root.val)&& validate(root.right, root.val, max);
     }
 
-    void inorder(TreeNode root){
-        if(root==null)return;
-        inorder(root.left);
-        ls.add(root.val);
-        inorder(root.right);
-    }
+    // List<Integer> ls= new ArrayList<>();
+    // public boolean isValidBST(TreeNode root){
+    //     inorder(root);
+    //     for(int i=0;i<ls.size()-1;i++){
+    //         if(ls.get(i)>=ls.get(i+1))return false;
+    //     }
+    //     return true;
+    // }
+
+    // void inorder(TreeNode root){
+    //     if(root==null)return;
+    //     inorder(root.left);
+    //     ls.add(root.val);
+    //     inorder(root.right);
+    // }
 
 }
