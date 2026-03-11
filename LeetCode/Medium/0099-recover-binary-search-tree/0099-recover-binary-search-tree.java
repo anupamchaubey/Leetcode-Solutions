f712de33d778c69916693c305128f2f311b6a729
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-12 01:10:39 +0530

    0099-recover-binary-search-tree .java

--- LeetCode/Medium/0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ LeetCode/Medium/0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -14,41 +14,27 @@
  * }
  */
 class Solution {
-    TreeNode node1 = null, node2 = null;
-    List<Integer> ls = new ArrayList<>();
-
+    TreeNode first = null, second = null, prev=null;
+    
     public void recoverTree(TreeNode root) {
 
-        inorder(root, -1, -1);
-        List<Integer> arr = new ArrayList<>(ls);
-        Collections.sort(arr);
-        int n1 = -1, n2 = -1;
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr.get(i) != ls.get(i)) {
-                if (n1 != -1) {
-                    n2 = ls.get(i);
-                    break;
-                } else
-                    n1 = ls.get(i);
-            }
-        }
-        inorder(root, n1, n2);
-        int temp = node1.val;
-        node1.val = node2.val;
-        node2.val = temp;
+        inorder(root);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
 
     }
 
-    void inorder(TreeNode root, int x, int y) {
+    void inorder(TreeNode root) {
         if (root == null)
             return;
-        inorder(root.left, x, y);
-        ls.add(root.val);
-        if (root.val == x)
-            node1 = root;
-        if (root.val == y)
-            node2 = root;
-        inorder(root.right, x, y);
+        inorder(root.left);
+        if(prev!=null && prev.val>root.val){
+            if(first==null)first=prev;
+            second=root;
+        }
+        prev=root;
+        inorder(root.right);
     }
 
 }
