86739160d2949b39a27f7c2c1a0bce8a1ed025db
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-11 14:22:59 +0530

    0968-binary-tree-cameras .java

--- LeetCode/Hard/0968-binary-tree-cameras/0968-binary-tree-cameras.java
+++ LeetCode/Hard/0968-binary-tree-cameras/0968-binary-tree-cameras.java
@@ -14,27 +14,24 @@
  * }
  */
 class Solution {
-    int camera = 0;
-
+    int camera=0;
     public int minCameraCover(TreeNode root) {
-        if (helper(root) == 0)
-             camera++;
+        if(helper(root)==0)camera++;
         return camera;
     }
-
-    int helper(TreeNode root) {
-        if (root == null) {
-            return 2;
-        }
-        int left = helper(root.left);
-        int right = helper(root.right);
-        if (left == 0 || right == 0) {
+    // 0 -> blank , 1 -> camera , 2-> covered
+    int helper(TreeNode root){
+        if(root==null)return 2;
+        int left=helper(root.left);
+        int right=helper(root.right);
+        if(left==0 || right==0){
             camera++;
             return 1;
         }
-        if (left == 1 || right == 1) {
+        else if(left==1|| right==1){
             return 2;
+        }else{
+            return 0;
         }
-        return 0;
     }
 }
