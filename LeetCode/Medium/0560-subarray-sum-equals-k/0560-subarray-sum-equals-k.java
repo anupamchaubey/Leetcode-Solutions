ac450b6c89a4c1b4270b7221ad09ccf23cc18788
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-12 10:31:00 +0530

    0560-subarray-sum-equals-k .java

--- LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,19 +1,18 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int n=nums.length;
-        int[] prefix=new int[n];
-        prefix[0]=nums[0];
-        HashSet<Integer> hs=new HashSet<>();
-        hs.add(0);
-        hs.add(nums[0]);
-        for(int i=1;i<n;i++){
-            prefix[i]=prefix[i-1]+nums[i];
-            hs.add(prefix[i]);
+        int n = nums.length;
+        int count = 0;
+        
+        HashMap<Integer, Integer> hm= new HashMap<>();
+        hm.put(0, 1);
+        int prefixsum=0;
+        for(int i=0;i<n;i++){
+            prefixsum+=nums[i];
+            hm.put(prefixsum, hm.getOrDefault(prefixsum, 0)+1);
+            if(hm.containsKey(prefixsum-k))count+=hm.get(prefixsum-k);
+            
         }
-        int count=0;
-        for(int i=0;i<prefix.length;i++){
-            if(hs.contains(prefix[i]-k))count++;
-        }
+
         return count;
 
     }
