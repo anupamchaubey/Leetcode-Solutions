f927ef8e6e51ca365514b55c9f7a8a0d97bfe6cf
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-12 13:22:17 +0530

    0974-subarray-sums-divisible-by-k .java

--- LeetCode/Medium/0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
+++ LeetCode/Medium/0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -1,18 +1,15 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
+        int n=nums.length;
+        HashMap<Integer, Integer> hm=new HashMap<>();
         hm.put(0, 1);
-        int val = 0;
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            val += nums[i];
-            int mod =val % k;
-            if (mod < 0)
-                mod += k;
-            if (hm.containsKey(mod)) {
-                count += hm.getOrDefault(mod, 0);
-            }
-            hm.put(mod, hm.getOrDefault(mod, 0) + 1);
+        int count=0;
+        int sum=0;
+        for(int x: nums){
+            sum+=x;
+            int rem=((sum % k) + k)%k;
+            if(hm.containsKey(rem))count+=hm.get(rem);
+            hm.put(rem, hm.getOrDefault(rem, 0)+1);
         }
         return count;
     }
