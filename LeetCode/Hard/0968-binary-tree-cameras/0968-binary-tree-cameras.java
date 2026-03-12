6fa7f9dd191eed176defffcd3f1e835a9ed64329
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-13 00:15:53 +0530

    0968-binary-tree-cameras .java

--- LeetCode/Hard/0968-binary-tree-cameras/0968-binary-tree-cameras.java
+++ LeetCode/Hard/0968-binary-tree-cameras/0968-binary-tree-cameras.java
@@ -13,37 +13,33 @@
  *     }
  * }
  */
-
- // Greedy Approach
+ // DP on tree Approach
 class Solution {
-    int camera;
+    class Pair{
+        int covered;
+        int empty;
+        int camera;
+        Pair(int covered, int empty, int camera){
+            this.covered=covered;
+            this.empty=empty;
+            this.camera=camera;
+        }
+    }
     public int minCameraCover(TreeNode root) {
-        camera=0;
-        
-        if(postorder(root)==0)camera++;
-        return camera;
+        Pair p=postorder(root);
+        return Math.min(p.covered, p.camera);
     }
-    // 0-> empty , 1-> camera , 2-> covered
-    int postorder(TreeNode root){
-        if(root==null)return 2;//already covered
-        int left=postorder(root.left);
-        int right=postorder(root.right);
-        //if any child is camera then this node will be automatically covered
-        
-        //if any child is empty then u must put camera here 
-        if(left==0 || right==0){
-            camera++;
-            return 1;
-        }
-        //order matters
-        else if(left==1|| right==1){
-            return 2;
-        
-        }
-        // all child are already covered
-        else{
-            return 0;
-        }
-    }
+    Pair postorder(TreeNode root){
+        if(root==null)return new Pair(0, Integer.MAX_VALUE/2, Integer.MAX_VALUE/2);
+        Pair left=postorder(root.left);
+        Pair right=postorder(root.right);
 
+        int co=Math.min(left.camera+ Math.min(right.camera, right.covered),right.camera+ Math.min(left.camera, left.covered));
+
+        int ca=1+Math.min(left.camera, Math.min(left.covered, left.empty))+Math.min(right.camera, Math.min(right.covered, right.empty));
+
+        int em=left.covered+right.covered;
+
+        return new Pair(co, em, ca);
+    }
 }
