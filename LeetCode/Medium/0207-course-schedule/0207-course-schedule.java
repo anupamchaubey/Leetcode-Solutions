d8668a3499395b591f523ee1fc7c177f4cdd66d4
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-15 16:56:03 +0530

    0207-course-schedule .java

--- LeetCode/Medium/0207-course-schedule/0207-course-schedule.java
+++ LeetCode/Medium/0207-course-schedule/0207-course-schedule.java
@@ -1,37 +1,41 @@
 class Solution {
     public boolean canFinish(int V, int[][] edges) {
-        List<List<Integer>> adj= new ArrayList<>();
-        for(int i=0;i<V;i++){
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
             adj.add(new ArrayList<>());
         }
-        
-        for(int[] e:edges){
+
+        for (int[] e : edges) {
             adj.get(e[0]).add(e[1]);
         }
-        boolean[] visited=new boolean[V];
-        boolean[] pathVisited=new boolean[V];
-        for(int i=0;i<V;i++){
-            if(!visited[i]){
-                
-                if(!dfs(i, adj, visited, pathVisited))return false;
-                
+        boolean[] visited = new boolean[V];
+        boolean[] pathVisited = new boolean[V];
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+
+                if (!dfs(i, adj, visited, pathVisited))
+                    return false;
+
             }
         }
         return true;
     }
-    public boolean dfs(int node, List<List<Integer>> adj, boolean[] visited, boolean[] pathVisited){
-        visited[i]=true;
-        pathVisited[i]=true;
-        for(int i: adj.get(node)){
-            if(!visited[i]){
-                
-                if(!dfs(i, adj, visited, pathVisited))return false;
-                
-            }else{
-                if(pathVisited[i])return false;
+
+    public boolean dfs(int node, List<List<Integer>> adj, boolean[] visited, boolean[] pathVisited) {
+        visited[node] = true;
+        pathVisited[node] = true;
+        for (int i : adj.get(node)) {
+            if (!visited[i]) {
+
+                if (!dfs(i, adj, visited, pathVisited))
+                    return false;
+
+            } else {
+                if (pathVisited[i])
+                    return false;
             }
         }
-        pathVisited[i]=false;
+        pathVisited[node] = false;
         return true;
     }
 }
