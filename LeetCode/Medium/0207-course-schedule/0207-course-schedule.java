a0d4f7dfd66608e34c76809430cbf38a046e992d
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-15 16:55:21 +0530

    0207-course-schedule .java

--- LeetCode/Medium/0207-course-schedule/0207-course-schedule.java
+++ LeetCode/Medium/0207-course-schedule/0207-course-schedule.java
@@ -12,26 +12,26 @@ class Solution {
         boolean[] pathVisited=new boolean[V];
         for(int i=0;i<V;i++){
             if(!visited[i]){
-                visited[i]=true;
-                pathVisited[i]=true;
+                
                 if(!dfs(i, adj, visited, pathVisited))return false;
-                pathVisited[i]=false;
+                
             }
         }
         return true;
     }
     public boolean dfs(int node, List<List<Integer>> adj, boolean[] visited, boolean[] pathVisited){
-
+        visited[i]=true;
+        pathVisited[i]=true;
         for(int i: adj.get(node)){
             if(!visited[i]){
-                visited[i]=true;
-                pathVisited[i]=true;
+                
                 if(!dfs(i, adj, visited, pathVisited))return false;
-                pathVisited[i]=false;
+                
             }else{
                 if(pathVisited[i])return false;
             }
         }
+        pathVisited[i]=false;
         return true;
     }
 }
