8237de62e27b7a089e6fe85b61a017604ccc881e
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-18 09:38:37 +0530

    0106-construct-binary-tree-from-inorder-and-postorder-traversal .java

--- LeetCode/Medium/0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ LeetCode/Medium/0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -1,27 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    int postorderIndex;
-    HashMap<Integer, Integer> hm = new HashMap<>();
-
+    HashMap<Integer, Integer> hm;
+    int postidx;
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        postorderIndex = postorder.length - 1;
-        for (int i = 0; i < inorder.length; i++) {
+        hm=new HashMap<>();
+        postidx=postorder.length-1;
+        for(int i=0;i<inorder.length;i++){
             hm.put(inorder[i], i);
         }
-        return rec(postorder, 0, postorder.length - 1);
+        return rec(postorder, 0, inorder.length-1);
     }
-
-    TreeNode rec(int[] postorder, int l, int r) {
-        if (l > r)
-            return null;
-        int rootVal = postorder[postorderIndex--];
-        TreeNode root = new TreeNode(rootVal);
-
-        int idx = hm.get(rootVal);
-        //why right first?
-        root.right = rec(postorder, idx + 1, r);
-        root.left = rec(postorder, l, idx - 1);
-
+    TreeNode rec(int[] postorder, int l, int r){
+        if(l>r)return null;
+        int rootVal=postorder[postidx--];
+        TreeNode root=new TreeNode(rootVal);
+        int idx=hm.get(rootVal);
+        root.right=rec(postorder, idx+1, r);
+        root.left=rec(postorder, l, idx-1);
         return root;
     }
-
 }
