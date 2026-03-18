2e410bbc6e8904f209374b655e8a1efb323f18d7
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-19 02:48:55 +0530

    0337-house-robber-iii .java

--- LeetCode/Medium/0337-house-robber-iii/0337-house-robber-iii.java
+++ LeetCode/Medium/0337-house-robber-iii/0337-house-robber-iii.java
@@ -14,28 +14,24 @@
  * }
  */
 class Solution {
-    class Pair {
+    class Pair{
         int rob;
-        int notrob;
-
-        Pair(int rob, int notrob) {
-            this.rob = rob;
-            this.notrob = notrob;
+        int not;
+        Pair(int rob, int not){
+            this.rob=rob;
+            this.not=not;
         }
     }
-
     public int rob(TreeNode root) {
-        Pair p = postorder(root);
-        return Math.max(p.rob, p.notrob);
+        Pair p=postorder(root);
+        return Math.max(p.rob, p.not);
     }
-
-    Pair postorder(TreeNode root) {
-        if (root == null)
-            return new Pair(0, 0);
-        Pair left = postorder(root.left);
-        Pair right = postorder(root.right);
-        int rob = root.val + left.notrob + right.notrob;
-        int notrob = Math.max(left.rob, left.notrob) + Math.max(right.rob, right.notrob);
-        return new Pair(rob, notrob);
+    Pair postorder(TreeNode root){
+        if(root==null)return new Pair(0, 0);
+        Pair left=postorder(root.left);
+        Pair right=postorder(root.right);
+        int rob=root.val+left.not+right.not;
+        int not=Math.max(left.rob, left.not)+Math.max(right.rob, right.not);
+        return new Pair(rob, not);
     }
 }
