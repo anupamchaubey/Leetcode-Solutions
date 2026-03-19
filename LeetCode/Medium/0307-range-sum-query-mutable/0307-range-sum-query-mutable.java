399032a18ade1d67f64428cc5bf3c8e071e273ca
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-19 10:29:28 +0530

    0307-range-sum-query-mutable .java

--- LeetCode/Medium/0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
+++ LeetCode/Medium/0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
@@ -1,58 +1,50 @@
 class NumArray {
     int[] tree;
     int n;
-
-    public NumArray(int[] nums) {
-        
-        this.n = nums.length;
-        tree = new int[4 * n];
-
-        build(nums, 0, 0, n - 1);
-    }
-
-    void build(int[] nums, int node, int start, int end) {
-        if (start == end) {
-            tree[node] = nums[start];
-        } else {
-            int mid = (start + end) / 2;
-            build(nums, 2 * node + 1, start, mid);
-            build(nums, 2 * node + 2, mid + 1, end);
-            tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+    void build(int[] nums, int node, int start, int end){
+        if(start==end){
+            tree[node]=nums[start];
+        }else{
+            int mid=(start+end)/2;
+            build(nums, 2*node+1, start, mid);
+            build(nums, 2*node+2, mid+1, end);
+            tree[node]=tree[2*node+1]+tree[2*node+2];
         }
-
     }
-
-    void updateq(int node, int start, int end, int idx, int val) {
-        if (start == end) {
-            tree[node] = val;
-        } else {
-            int mid = (start + end) / 2;
-            if (idx <= mid) {
-                updateq(2 * node + 1, start, mid, idx, val);
-            } else {
-                updateq(2 * node + 2, mid + 1, end, idx, val);
+    void update(int node, int start, int end, int idx, int val){
+        if(start==end){
+            tree[node]=val;
+        }
+        else{
+            int mid=(start+end)/2;
+            if(idx<=mid){
+                update(2*node+1, start, mid, idx, val);
+            }else{
+                update(2*node+2, mid+1, end, idx, val);
             }
-            tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+            tree[node]=tree[2*node+1]+tree[2*node+2];
         }
     }
-
-    int query(int node, int start, int end, int l, int r) {
-        if (r < start || l > end)
-            return 0;
-        if (l <= start && end <= r)
-            return tree[node];
-        int mid = (start + end) / 2;
-        int left = query(2 * node + 1, start, mid, l, r);
-        int right = query(2 * node + 2, mid + 1, end, l, r);
-        return left + right;
+    int query(int node, int start, int end, int l, int r){
+        if(l>end || r<start)return 0;
+        if(start>=l && end<=r)return tree[node];
+        int mid=(start+end)/2;
+        int left=query(2*node+1, start, mid, l, r);
+        int right=query(2*node+2, mid+1, end, l, r);
+        return left+right;
     }
-
+    public NumArray(int[] nums) {
+        n=nums.length;
+        tree=new int[4*n];
+        build(nums,0, 0, n-1);
+    }
+    
     public void update(int index, int val) {
-        updateq(0, 0, n - 1, index, val);
+        update(0, 0, n-1, index, val);
     }
-
+    
     public int sumRange(int left, int right) {
-        return query(0, 0, n - 1, left, right);
+        return query(0, 0, n-1, left, right);
     }
 }
 
