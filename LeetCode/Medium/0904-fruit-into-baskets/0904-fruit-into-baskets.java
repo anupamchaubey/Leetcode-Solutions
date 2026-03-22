583734c5f28eaf3fda423a10dc0b31d22ff21251
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-22 21:40:43 +0530

    0904-fruit-into-baskets .java

---
+++ LeetCode/Medium/0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        return atmost(fruits, 2);
+    }
+    int atmost(int[] fruits, int k){
+        HashMap<Integer, Integer> hm=new HashMap<>();
+        int count=0;
+        int left=0;
+        int max=0;
+        for(int r=0;r<fruits.length;r++){
+            hm.put(fruits[r], hm.getOrDefault(fruits[r], 0)+1);
+
+            while(hm.size()>2){
+                hm.put(fruits[left], hm.get(fruits[left])-1);
+                if(hm.get(fruits[left])==0)hm.remove(fruits[left]);
+                left++;
+            }
+            count=r-left+1;
+            max=Math.max(max, count);
+        }
+        return max;
+    }
+}
