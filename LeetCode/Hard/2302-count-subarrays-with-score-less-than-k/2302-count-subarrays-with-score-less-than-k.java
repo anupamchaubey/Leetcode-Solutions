0a2e9301bd77b305bedc90d3ff6851efc64a7e69
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-22 20:35:37 +0530

    2302-count-subarrays-with-score-less-than-k .java

--- LeetCode/Hard/2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.java
+++ LeetCode/Hard/2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.java
@@ -1,22 +1,23 @@
 class Solution {
     public long countSubarrays(int[] nums, long k) {
-        return atmost(nums, k);
+        return atmost(nums, k-1);
     }
-    long atmost(int[] nums, long k){
-        long score=0;
-        long sum=0;
-        long count=0;
-        int left=0;
-        for(int r=0;r<nums.length;r++){
-            sum+=nums[r];
-            score=sum*(r-left+1);
 
-            while(score>=k){
-                sum-=nums[left];
+    long atmost(int[] nums, long k) {
+        long score = 0;
+        long sum = 0;
+        long count = 0;
+        int left = 0;
+        for (int r = 0; r < nums.length; r++) {
+            sum += nums[r];
+            score = sum * (r - left + 1);
+
+            while (score > k) {
+                sum -= nums[left];
                 left++;
-                score=sum*(r-left+1);
+                score = sum * (r - left + 1);
             }
-            count+=(r-left+1);
+            count += (r - left + 1);
         }
         return count;
     }
