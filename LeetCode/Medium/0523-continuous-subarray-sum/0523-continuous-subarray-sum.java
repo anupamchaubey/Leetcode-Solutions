3d2ed572401c290f540151514437e3177eff4261
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-24 00:04:29 +0530

    0523-continuous-subarray-sum .java

--- LeetCode/Medium/0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ LeetCode/Medium/0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -5,13 +5,9 @@ class Solution {
         int sum=0;
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
-            int rem=0;
-            if(k==0)rem=sum;
-            else rem=((sum%k)+k)%k;
+            int rem=(k==0)? sum: ((sum%k)+k)%k;
             if(hm.containsKey(rem) && i-hm.get(rem)>=2)return true;
-            if(!hm.containsKey(rem)){
-                hm.put(rem, i);
-            }
+            if(!hm.containsKey(rem))hm.put(rem, i);
         }
         return false;
     }
