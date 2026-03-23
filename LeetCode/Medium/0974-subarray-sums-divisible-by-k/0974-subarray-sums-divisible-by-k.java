f2b136da1b78f8c7445f865b00c09e91f25a1e40
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-23 23:03:43 +0530

    0974-subarray-sums-divisible-by-k .java

--- LeetCode/Medium/0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
+++ LeetCode/Medium/0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -1,5 +1,23 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
+        /*
+Count subarrays whose sum is divisible by k
+
+💡 CORE IDEA
+
+If:
+
+prefix[i] % k == prefix[j] % k
+
+👉 Then subarray (i+1 → j) is divisible by k
+
+🔥 WHY?
+
+Because:
+
+(prefix[j] - prefix[i]) % k = 0
+        */
+        
         HashMap<Integer, Integer> hm= new HashMap<>();
         hm.put(0, 1);
         int count=0;
@@ -10,7 +28,7 @@ class Solution {
             if(hm.containsKey(rem)){
                 count+=hm.get(rem);
             }
-            hm.put(rem, hm.getOrDefault(rem,0)+1);
+            hm.put(rem, hm.getOrDefault(rem, 0)+1);
         }
         return count;
     }
