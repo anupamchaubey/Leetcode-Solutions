8b7e2bd0561093d1e24f5a0e313842b90d895de7
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-23 23:48:45 +0530

    0523-continuous-subarray-sum .java

--- LeetCode/Medium/0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ LeetCode/Medium/0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -1,13 +1,17 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
         HashMap<Integer, Integer> hm= new HashMap<>();
-        hm.put(0, 1);
+        hm.put(0, -1);
         int sum=0;
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
-            int rem=((sum%k)+k)%k;
+            int rem=0;
+            if(k==0)rem=sum;
+            else rem=((sum%k)+k)%k;
             if(hm.containsKey(rem) && i-hm.get(rem)>=2)return true;
-            hm.put(rem, i);
+            if(!hm.containsKey(rem)){
+                hm.put(rem, i);
+            }
         }
         return false;
     }
