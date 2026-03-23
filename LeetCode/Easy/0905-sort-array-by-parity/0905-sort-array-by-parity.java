dc6fa40d5628382d40e9e8925108df6176849d24
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-23 12:36:05 +0530

    0905-sort-array-by-parity .java

---
+++ LeetCode/Easy/0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int left=0, right=nums.length-1;
+        while(left<right){
+            while(left<right && nums[left]%2==0)left++;
+            while(right>left && nums[right]%2!=0)right--;
+            int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+            left++;
+            right--;
+        }
+        return nums;
+    }
+}
