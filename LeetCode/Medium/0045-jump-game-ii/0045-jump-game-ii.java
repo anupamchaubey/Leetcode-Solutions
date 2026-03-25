3f538ac8ec6d47f5f2cd9889c3dcf4710d7c282e
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-26 01:19:40 +0530

    0045-jump-game-ii .java

--- LeetCode/Medium/0045-jump-game-ii/0045-jump-game-ii.java
+++ LeetCode/Medium/0045-jump-game-ii/0045-jump-game-ii.java
@@ -3,17 +3,17 @@ class Solution {
     //WHEN U REACH THE END OF CURRENT JUMP RANGE, U MUST JUMP AND EXTEND THE RANGE TO THE FARTHEST REACHABLE INDEX
     
     public int jump(int[] nums) {
-        int jumps=0;
-        int end=0;
-        int farthest=0;
         //NEVER NEED TO JUMP FROM LAST INDEX SO TILL N-2
-        for(int i=0;i<nums.length-1;i++){
-            farthest=Math.max(farthest, i+nums[i]);
-
-            if(i==end){
+        int jumps=0;
+        int farthest=0;
+        int currEnd=0;
+        for(int i=0;i<nums.length;i++){
+            
+            if(i>currEnd){
                 jumps++;
-                end=farthest;
+                currEnd=farthest;
             }
+            farthest=Math.max(farthest, i+nums[i]);
         }
         return jumps;
     }
