e28adb22bedac4be7f389f33f41da889c593a953
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-25 22:32:35 +0530

    0881-boats-to-save-people .java

--- LeetCode/Medium/0881-boats-to-save-people/0881-boats-to-save-people.java
+++ LeetCode/Medium/0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,17 +1,16 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
         Arrays.sort(people);
-        int boats = 0;
-        int i = 0, j = people.length - 1;
-        while (i <= j) {
-            if (people[i] + people[j] <= limit) {
+        int boats=0;
+        int i=0, j=people.length-1;
+        while(i<=j){
+            if(people[i]+people[j]<=limit){
+                boats++;
                 i++;
                 j--;
-                
+            }else{
                 boats++;
-            } else {
                 j--;
-                boats++;
             }
         }
         return boats;
