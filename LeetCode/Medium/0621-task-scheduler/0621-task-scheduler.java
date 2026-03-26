228a0559d248e741b3fcfcc2c219c7581ca727b6
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-26 15:07:10 +0530

    0621-task-scheduler .java

--- LeetCode/Medium/0621-task-scheduler/0621-task-scheduler.java
+++ LeetCode/Medium/0621-task-scheduler/0621-task-scheduler.java
@@ -1,15 +1,32 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int maxfreq=0;
-        int maxcount=0;
-        int[] freq= new int[26];
-        for(char ch: tasks){
-            freq[ch-'A']++;
-            maxfreq=Math.max(maxfreq, freq[ch-'A']);
+        int[] freq = new int[26];
+        for (char ch : tasks) {
+            freq[ch - 'A']++;
         }
-        for(int f: freq){
-            if(f==maxfreq)maxcount++;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b)-> b-a);
+        for (int f : freq) {
+            if (f > 0)
+                pq.offer(f);
         }
-        return Math.max((maxfreq-1)*(n+1)+maxcount, tasks.length);
+        int time = 0;
+        while (!pq.isEmpty()) {
+            int gap = n + 1;
+            List<Integer> temp = new ArrayList<>();
+            while (gap > 0 && !pq.isEmpty()) {
+                int x = pq.poll();
+                x--;
+                gap--;
+                time++;
+                if (x > 0)
+                    temp.add(x);
+            }
+            for (int x : temp)
+                pq.offer(x);
+            if (pq.isEmpty())
+                break;
+            time += gap;
+        }
+        return time;
     }
 }
