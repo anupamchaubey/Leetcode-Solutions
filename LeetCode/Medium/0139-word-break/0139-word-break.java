5f13b4f3650d53979272bcef20cac2fa1e7e226d
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-27 10:34:08 +0530

    0139-word-break .java

---
+++ LeetCode/Medium/0139-word-break/0139-word-break.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> dict) {
+        int n=s.length();
+        Set<String> hs= new HashSet<>();
+        for(String str: dict)hs.add(str);
+        boolean[] dp= new boolean[n+1];
+        dp[0]=true;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<i;j++){
+                if(dp[j] && hs.contains(s.substring(j, i)))dp[i]=true;
+            }
+        }
+        return dp[n];
+    }
+}
