071b8b23475665e9def890cd4b42af0f9f9b1566
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-27 10:41:47 +0530

    0139-word-break .java

--- LeetCode/Medium/0139-word-break/0139-word-break.java
+++ LeetCode/Medium/0139-word-break/0139-word-break.java
@@ -1,16 +1,28 @@
 class Solution {
-    // prefix dp
+    // // prefix dp
     public boolean wordBreak(String s, List<String> dict) {
+    //     int n=s.length();
+    //     Set<String> hs= new HashSet<>();
+    //     for(String str: dict)hs.add(str);
+    //     boolean[] dp= new boolean[n+1];
+    //     dp[0]=true;
+    //     for(int i=1;i<=n;i++){
+    //         for(int j=0;j<i;j++){
+    //             if(dp[j] && hs.contains(s.substring(j, i)))dp[i]=true;
+    //         }
+    //     }
+    //     return dp[n];
         int n=s.length();
         Set<String> hs= new HashSet<>();
         for(String str: dict)hs.add(str);
-        boolean[] dp= new boolean[n+1];
-        dp[0]=true;
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<i;j++){
-                if(dp[j] && hs.contains(s.substring(j, i)))dp[i]=true;
-            }
+        return rec(s, 0, 1, hs);
+    }
+    boolean rec(String s, int i, int j, Set<String> hs){
+        if(i==s.length())return true;
+        if (j > s.length()) return false;
+        if(hs.contains(s.substring(i, j))){
+            return rec(s, j, j+1, hs)|| rec(s, i, j+1, hs);
         }
-        return dp[n];
+        else return rec(s, i, j+1, hs);
     }
 }
