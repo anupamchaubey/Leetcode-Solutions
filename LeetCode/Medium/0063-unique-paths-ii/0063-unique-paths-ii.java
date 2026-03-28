c8cbbbc160677b050a0e725cae5d74596122fe56
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-28 18:22:42 +0530

    0063-unique-paths-ii .java

--- LeetCode/Medium/0063-unique-paths-ii/0063-unique-paths-ii.java
+++ LeetCode/Medium/0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,26 +1,30 @@
 class Solution {
     int m;
     int n;
+
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        m=obstacleGrid.length;
-        n=obstacleGrid[0].length;
-        if(obstacleGrid[0][0]==1 || obstacleGrid[m-1][n-1]==1)return 0;
-        int[][] dp= new int[m][n];
-        dp[m-1][n-1]=1;
-        for(int i=m-1;i>=0;i--){
-            if(obstacleGrid[i][n-1]==0){
-                dp[i][n-1]=1;
-            }
+        m = obstacleGrid.length;
+        n = obstacleGrid[0].length;
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1)
+            return 0;
+        int[][] dp = new int[m][n];
+        
+        for (int i = m - 1; i >= 0; i--) {
+            if (obstacleGrid[i][n - 1] == 0) {
+                dp[i][n - 1] = 1;
+            } else
+                break;
         }
-        for(int i=n-1;i>=0;i--){
-            if(obstacleGrid[m-1][i]==0){
-                dp[m-1][i]=1;
-            }
+        for (int i = n - 1; i >= 0; i--) {
+            if (obstacleGrid[m - 1][i] == 0) {
+                dp[m - 1][i] = 1;
+            } else
+                break;
         }
-        for(int i=m-2;i>=0;i--){
-            for(int j=n-2;j>=0;j--){
-                if(obstacleGrid[i][j]!=1){
-                    dp[i][j]=dp[i+1][j]+dp[i][j+1];
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                if(obstacleGrid[i][j]==0){
+                    dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
                 }
             }
         }
@@ -28,14 +32,16 @@ class Solution {
 
         // return rec(obstacleGrid, 0, 0);
     }
-    int rec(int[][] obs, int i, int j){
-        if(i==m-1 && j==n-1)return 1;
-        int c=0;
-        if(i+1<m && obs[i+1][j]!=1){
-            c+=rec(obs, i+1, j);
+
+    int rec(int[][] obs, int i, int j) {
+        if (i == m - 1 && j == n - 1)
+            return 1;
+        int c = 0;
+        if (i + 1 < m && obs[i + 1][j] != 1) {
+            c += rec(obs, i + 1, j);
         }
-        if(j+1<n && obs[i][j+1]!=1){
-            c+=rec(obs, i, j+1);
+        if (j + 1 < n && obs[i][j + 1] != 1) {
+            c += rec(obs, i, j + 1);
         }
         return c;
     }
