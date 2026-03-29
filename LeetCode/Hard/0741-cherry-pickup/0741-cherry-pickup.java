4cc25750a29e8a284726248a0335daf61efa345a
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-29 16:49:01 +0530

    0741-cherry-pickup .java

--- LeetCode/Hard/0741-cherry-pickup/0741-cherry-pickup.java
+++ LeetCode/Hard/0741-cherry-pickup/0741-cherry-pickup.java
@@ -1,34 +1,30 @@
 class Solution {
-    int n;
-    Integer[][][][] dp;
-
+    int m, n;
     public int cherryPickup(int[][] grid) {
-        n = grid.length;
-        dp = new Integer[n][n][n][n];
-        if (grid[0][0] == -1 || grid[n - 1][n - 1] == -1)
-            return 0;
-        return Math.max(0, pick(grid, 0, 0, 0));
+        m=grid.length;
+        n=grid[0].length;
+        Integer[][][][] dp= new Integer[m][n][m][n];
+        return Math.max(0, rec(grid, 0, 0, 0, 0, dp));
     }
-
-    int pick(int[][] grid, int r1, int c1, int r2) {
-        if (r1 == n - 1 && c1 == n - 1)
-            return grid[r1][c1];
-        int c2 = r1 + c1 - r2;
-        if (r1 >= n || c1 >= n || r2 >= n || c2 >= n || grid[r1][c1] == -1 || grid[r2][c2] == -1)
-            return Integer.MIN_VALUE / 2;
-        if (dp[r1][c1][r2][c2] != null)
-            return dp[r1][c1][r2][c2];
-        int cherries = 0;
-        if (r1 == r2 && c1 == c2) {
-            cherries += grid[r1][c1];
-        } else {
-            cherries += grid[r1][c1] + grid[r2][c2];
+    int rec(int[][] grid, int i, int j, int p, int q, Integer[][][][] dp){
+        if(i==m-1 && j==n-1 && p==m-1 && q==n-1)return grid[i][j];
+        if(i>=m || j>=n || p>=m || q>=n)return Integer.MIN_VALUE;
+        if(grid[i][j]==-1 || grid[p][q]==-1)return Integer.MIN_VALUE;
+        
+        if(dp[i][j][p][q]!=null)return dp[i][j][p][q];
+        int cherries=0;
+        if(i==p && j==q){
+            cherries+=grid[i][j];
+        }else{
+            cherries+=grid[i][j];
+            cherries+=grid[p][q];
         }
-        int p1 = pick(grid, r1 + 1, c1, r2 + 1);
-        int p2 = pick(grid, r1, c1 + 1, r2 + 1);
-        int p3 = pick(grid, r1, c1 + 1, r2);
-        int p4 = pick(grid, r1 + 1, c1, r2);
-        cherries += Math.max(Math.max(p1, p2), Math.max(p3, p4));
-        return dp[r1][c1][r2][c2] = cherries;
+        int max=Integer.MIN_VALUE;
+        max=Math.max(max, rec(grid, i+1, j, p+1, q, dp));
+        max=Math.max(max, rec(grid, i, j+1, p, q+1, dp));
+        max=Math.max(max, rec(grid, i+1, j, p, q+1, dp));
+        max=Math.max(max, rec(grid, i, j+1, p+1, q, dp));
+        cherries+=max;
+        return dp[i][j][p][q]=cherries;
     }
 }
