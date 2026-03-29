35f90ab71c9113265601a61a2dcec67989822d1e
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-29 17:49:45 +0530

    0300-longest-increasing-subsequence .java

--- LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,25 +1,18 @@
 class Solution {
-    Integer[][] dp;
-
     public int lengthOfLIS(int[] nums) {
-        dp = new Integer[nums.length][nums.length];
-        return rec(nums, -1, 0);
-    }
-
-    int rec(int[] nums, int prev, int idx) {
-        if (idx == nums.length)
-            return 0;
-        if (prev != -1 && dp[prev][idx] != null)
-            return dp[prev][idx];
-        int take = 0;
-        if (prev == -1 || nums[idx] > nums[prev]) {
-            take = 1 + rec(nums, idx, idx + 1);
+        int[] dp= new int[nums.length];
+        Arrays.fill(dp, 1);
+        int max=1;
+        for(int i=1;i<nums.length;i++){
+            
+            for(int prev=0;prev<i;prev++){
+                
+                if(nums[i]> nums[prev]){
+                    dp[i]=Math.max(dp[i], dp[prev]+1);
+                }
+            }
+            max=Math.max(dp[i], max);
         }
-        int nontake = rec(nums, prev, idx + 1);
-        int ans = Math.max(take, nontake);
-        if (prev != -1) {
-            dp[prev][idx] = ans;
-        }
-        return ans;
+        return max;
     }
 }
