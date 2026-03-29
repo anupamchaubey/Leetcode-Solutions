ba496cdd44f90d3c3c079e2bd872bd96655e7342
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-29 17:51:41 +0530

    0300-longest-increasing-subsequence .java

--- LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,10 +1,9 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int[] dp= new int[nums.length];
-        Arrays.fill(dp, 1);
         int max=1;
-        for(int i=1;i<nums.length;i++){
-            
+        for(int i=0;i<nums.length;i++){
+            dp[i]=1;
             for(int prev=0;prev<i;prev++){
                 
                 if(nums[i]> nums[prev]){
