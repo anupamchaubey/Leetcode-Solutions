c841edbf48b5ee326b768a6b37bf8d42b0dc6c85
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-29 20:22:21 +0530

    0354-russian-doll-envelopes .java

--- LeetCode/Hard/0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
+++ LeetCode/Hard/0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
@@ -1,29 +1,28 @@
 class Solution {
-    public int maxEnvelopes(int[][] envelopes) {
-        int n = envelopes.length;
-        Arrays.sort(envelopes, (a, b) -> {
-            if (a[0] == b[0])
-                return b[1] - a[1];
-            return a[0] - b[0];
+    public int maxEnvelopes(int[][] env) {
+        int n=env.length;
+        Arrays.sort(env, (a, b)-> {
+            if(a[0]==b[0]){
+                return b[1]-a[1];
+            }return a[0]-b[0];
         });
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
-        int size = 0;
-        for (int[] v : envelopes) {
-            int l = 0, r = size;
-            while (l < r) {
-                int mid = (l + r) / 2;
-                if (dp[mid] < v[1]) {
-                    l = mid + 1;
-                } else {
-                    r = mid;
+
+        int max=0;
+        int size=0;
+        int[] tails=new int[n];
+        for(int i=0;i<n;i++){
+            int l=0,r=size;
+            while(l<r){
+                int mid=l+(r-l)/2;
+                if(tails[mid]<env[i][1]){
+                    l=mid+1;
+                }else{
+                    r=mid;
                 }
             }
-            dp[l] = v[1];
-            if (l == size)
-                size++;
+            tails[l]=env[i][1];
+            if(l==size)size++;
         }
-
         return size;
     }
 }
