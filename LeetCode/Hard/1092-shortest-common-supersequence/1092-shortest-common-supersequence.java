c313354e7f10ea4e5dc3b24d92c92af3564f972e
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-30 10:43:12 +0530

    1092-shortest-common-supersequence .java

--- LeetCode/Hard/1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ LeetCode/Hard/1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -1,43 +1,43 @@
 class Solution {
-    public String shortestCommonSupersequence(String str1, String str2) {
-        int m=str1.length();
-        int n=str2.length();
-        int[][] lcs=new int[m+1][n+1];
+    public String shortestCommonSupersequence(String s1, String s2) {
+        int m=s1.length();
+        int n=s2.length();
+
+        int[][] dp= new int[m+1][n+1];
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
-                if(str1.charAt(i-1)==str2.charAt(j-1)){
-                    lcs[i][j]=1+lcs[i-1][j-1];
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
                 }else{
-                    lcs[i][j]=Math.max(lcs[i-1][j], lcs[i][j-1]);
+                    dp[i][j]=Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
+        StringBuilder sb=new StringBuilder();
         int i=m, j=n;
-        StringBuilder sb= new StringBuilder();
         while(i>0 && j>0){
-            if(str1.charAt(i-1)==str2.charAt(j-1)){
-                sb.append(str1.charAt(i-1));
+            if(s1.charAt(i-1)==s2.charAt(j-1)){
+                sb.append(s1.charAt(i-1));
                 i--;
                 j--;
+            }else if(dp[i-1][j]>dp[i][j-1]){
+                sb.append(s1.charAt(i-1));
+                i--;
             }else{
-                if(lcs[i-1][j]>lcs[i][j-1]){
-                    sb.append(str1.charAt(i-1));
-                    i--;
-                }else{
-                    sb.append(str2.charAt(j-1));
-                    j--;
-                }
+                sb.append(s2.charAt(j-1));
+                j--;
             }
         }
         while(i>0){
-            sb.append(str1.charAt(i-1));
+            sb.append(s1.charAt(i-1));
             i--;
         }
         while(j>0){
-            sb.append(str2.charAt(j-1));
+            sb.append(s2.charAt(j-1));
             j--;
         }
         sb.reverse();
         return sb.toString();
+
     }
 }
