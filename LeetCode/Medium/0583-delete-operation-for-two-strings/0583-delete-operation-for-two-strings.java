1d75ca62c2b6177990f10d68c6152b3d46ea5611
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-03-31 22:45:31 +0530

    0583-delete-operation-for-two-strings .java

---
+++ LeetCode/Medium/0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minDistance(String s1, String s2) {
+        return rec(s1, s2, 0, 0);
+    }
+    int rec(String s1, String s2, int i, int j){
+        if(i==s1.length()){
+            return s2.length()-j;
+        }
+        if(j==s2.length()){
+            return s1.length()-i;
+        }
+        int min;
+        if(s1.charAt(i)==s2.charAt(j)){
+            min = rec(s1, s2, i+1, j+1);
+        }else{
+            int x=1+rec(s1, s2, i+1, j);
+            int y=1+rec(s1, s2, i, j+1);
+            min=Math.min(x, y);
+        }
+        return min;
+    }
+}
