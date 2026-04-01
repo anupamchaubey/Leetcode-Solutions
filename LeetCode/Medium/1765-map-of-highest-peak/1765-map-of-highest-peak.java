ebbfc49eae6f401848259e6d318be51306ebfacc
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-01 22:21:40 +0530

    1765-map-of-highest-peak .java

--- LeetCode/Medium/1765-map-of-highest-peak/1765-map-of-highest-peak.java
+++ LeetCode/Medium/1765-map-of-highest-peak/1765-map-of-highest-peak.java
@@ -1,40 +1,40 @@
 class Solution {
-    public int[][] highestPeak(int[][] isWater) {
-        int m=isWater.length;
-        int n=isWater[0].length;
-        
+    public int[][] highestPeak(int[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        int[] row={-1, 1, 0, 0};
+        int[] col={0, 0, -1, 1};
+        int[][] ans=new int[m][n];
+        Queue<int[]> q= new LinkedList<>();
         boolean[][] visited=new boolean[m][n];
-
-        Queue<int[]> q=new LinkedList();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(isWater[i][j]==1){
-                    
+                if(grid[i][j]==1){
+                    q.offer(new int[] {i, j});
                     visited[i][j]=true;
-                    q.offer(new int[]{i, j});
                 }
             }
         }
-        int[][] dir={{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-        int level=0;
+        
+        int d=0;
         while(!q.isEmpty()){
             int size=q.size();
             for(int p=0;p<size;p++){
                 int[] arr=q.poll();
                 int r=arr[0];
                 int c=arr[1];
-                isWater[r][c]=level;
+                ans[r][c]=d;
                 for(int i=0;i<4;i++){
-                    int nr=r+dir[i][0];
-                    int nc=c+dir[i][1];
-                    if(nr>=0 && nr<m && nc>=0 && nc<n && !visited[nr][nc]){
+                    int nr=r+row[i];
+                    int nc=c+col[i];
+                    if(nr>=0 && nc>=0 && nr<m && nc<n && !visited[nr][nc]){
                         visited[nr][nc]=true;
                         q.offer(new int[]{nr, nc});
                     }
                 }
             }
-            level++;
+            d++;
         }
-        return isWater;
+        return ans;
     }
 }
