3fd75e8a9121692015ac627ec67d466cccca02a9
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-01 08:47:47 +0530

    0132-palindrome-partitioning-ii .java

--- LeetCode/Hard/0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ LeetCode/Hard/0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,32 +1,36 @@
 class Solution {
-    // prefix dp
+    boolean[][] pal;
+
     public int minCut(String s) {
-        int n=s.length();
-        boolean[][] pal= new boolean[n+1][n+1];
-        for(int i=n-1;i>=0;i--){
-            for(int j=i+1;j<=n;j++){
-                int len=j-i;
-                if(len==1){
-                    pal[i][j]=true;
-                }else if(len==2){
-                    pal[i][j]=(s.charAt(i)==s.charAt(j-1));
-                }else{
-                    if(pal[i+1][j-1] && s.charAt(i)==s.charAt(j-1)){
-                        pal[i][j]=true;
-                    }
+        int n = s.length();
+        pal = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            pal[i][i] = true;
+        }
+        for (int j = 0; j < n; j++) {
+            for (int i = j - 1; i >= 0; i--) {
+                int len = j - i + 1;
+                if (len == 2) {
+                    pal[i][j] = (s.charAt(i) == s.charAt(j));
+                } else {
+                    pal[i][j] = (s.charAt(i) == s.charAt(j)) && pal[i + 1][j - 1];
                 }
             }
         }
-        int[] dp= new int[n+1];
-        dp[0]=-1;
-        for(int i=1;i<=n;i++){
-            dp[i]=Integer.MAX_VALUE;
-            for(int j=0;j<i;j++){
-                if(pal[j][i]){
-                    dp[i]=Math.min(dp[i], dp[j]+1);
-                }
+        return rec(s, 0)-1;
+    }
+
+    int rec(String s, int st) {
+        if (st >= s.length())
+            return 0;
+        
+        int min=Integer.MAX_VALUE;
+        for(int i=st;i<s.length();i++){
+            if(pal[st][i]){
+                min=Math.min(min, 1+rec(s, i+1));
             }
         }
-        return dp[n];
+        return min;
     }
 }
