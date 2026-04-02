62e00d848b13b00bc08fffb9ddbc6e5a7050961b
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-02 23:55:56 +0530

    0684-redundant-connection .java

--- LeetCode/Medium/0684-redundant-connection/0684-redundant-connection.java
+++ LeetCode/Medium/0684-redundant-connection/0684-redundant-connection.java
@@ -1,43 +1,37 @@
 class Solution {
-
-    //using Disjoint Union cycle detection
-
-    int[] parent, rank;
-
-    public int[] findRedundantConnection(int[][] edges) {
-        int n = edges.length;
-        parent = new int[n + 1];
-        rank = new int[n + 1];
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-            rank[i] = 0;
-        }
-        for (int[] e : edges) {
-            if (!union(e[0], e[1]))
-                return e;
-        }
-        return new int[] {};
+    int[] parent;
+    int[] size;
+    
+    int find(int x){
+        if(parent[x]!=x)return find(parent[x]);
+        return x;
     }
-
-    int find(int x) {
-        if (parent[x] == x)
-            return x;
-        return parent[x] = find(parent[x]);
-    }
-
-    boolean union(int x, int y) {
-        int px = find(x);
-        int py = find(y);
-        if (px == py)
-            return false;
-        if (rank[px] > rank[py]) {
-            parent[py] = px;
-        } else if (rank[py] > rank[px]) {
-            parent[px] = py;
-        } else {
-            parent[py] = px;
-            rank[px]++;
+    boolean union(int x, int y){
+        int px=find(x);
+        int py=find(y);
+        if(px==py)return false;
+        if(size[px]>size[py]){
+            parent[py]=px;
+        }else if(size[py]>size[px]){
+            parent[px]=py;
+        }else{
+            parent[px]=py;
+            size[py]+=size[px];
         }
         return true;
+    }
+    public int[] findRedundantConnection(int[][] edges) {
+        parent=new int[edges.length];
+        size=new int[edges.length];
+        for(int i=0;i<edges.length;i++){
+            parent[i]=i;
+        }
+        int[] ans={-1, -1};
+        for(int[] e: edges){
+            if(!union(e[0]-1, e[1]-1)){
+                ans=e;
+            }
+        }
+        return ans;
     }
 }
