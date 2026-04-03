c0310cf667c4ebee9a4469eb1523ea4acf9de3bb
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-03 19:39:55 +0530

    0743-network-delay-time .java

--- LeetCode/Medium/0743-network-delay-time/0743-network-delay-time.java
+++ LeetCode/Medium/0743-network-delay-time/0743-network-delay-time.java
@@ -20,31 +20,33 @@ class Solution {
             int ti = t[2];
             adj.get(u).add(new Pair(v, ti));
         }
-        int[] dist=new int[n+1];
-        Arrays.fill(dist, Integer.MAX_VALUE/2);
-        dist[0]=-1;
-        dist[k]=0;
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE / 2);
+        dist[0] = -1;
+        dist[k] = 0;
 
         PriorityQueue<Pair> q = new PriorityQueue<>((a, b) -> Integer.compare(a.t, b.t));
-        
+
         q.offer(new Pair(k, 0));
 
         while (!q.isEmpty()) {
             Pair p = q.poll();
             int u = p.v;
-
+            if(p.t>dist[u])continue;
             for (Pair pv : adj.get(u)) {
                 int v = pv.v;
-                if(dist[u]+pv.t<dist[v]){
-                    dist[v]=dist[u]+pv.t;
+                if (dist[u] + pv.t < dist[v]) {
+                    dist[v] = dist[u] + pv.t;
                     q.offer(new Pair(v, dist[v]));
                 }
-                
+
             }
         }
-        int max=0;
-        for(int x: dist)max=Math.max(max, x);
-        if(max==Integer.MAX_VALUE/2)return -1;
+        int max = 0;
+        for (int x : dist)
+            max = Math.max(max, x);
+        if (max == Integer.MAX_VALUE / 2)
+            return -1;
 
         return max;
     }
