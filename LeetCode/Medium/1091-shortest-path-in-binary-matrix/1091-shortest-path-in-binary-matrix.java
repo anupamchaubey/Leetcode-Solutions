bb623067f9cfbc9e0b416782c2ad285f9e45b1bb
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-03 19:14:17 +0530

    1091-shortest-path-in-binary-matrix .java

--- LeetCode/Medium/1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ LeetCode/Medium/1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,38 +1,43 @@
 class Solution {
-    class Pair{
-        int r;
-        int c;
-        int d;
-        Pair(int r, int c, int d){
-            this.r=r;
-            this.c=c;
-            this.d=d;
-        }
-    }
-    int[] dr={-1, -1, -1, 0, 0, 1, 1, 1};
-    int[] dc={-1, 0, 1, -1, 1, -1, 0, 1};
+    int m;
+    int n;
+    int[] dr = { -1, -1, -1, 0, 0, 1, 1, 1 };
+    int[] dc = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-        if(grid[0][0]!=0)return -1;
-        boolean[][] visited= new boolean[m][n];
-        Queue<Pair> q= new ArrayDeque<>();
-        visited[0][0]=true;
-        q.offer(new Pair(0, 0, 1));
-        while(!q.isEmpty()){
-            Pair p=q.poll();
-            int row=p.r;
-            int col=p.c;
-            int dist=p.d;
-            if(row==m-1 && col==n-1)return dist;
-            for(int i=0;i<8;i++){
-                int r=row+dr[i];
-                int c=col+dc[i];
-                if(r>=0 && c>=0 && r<m && c<n && grid[r][c]==0 && !visited[r][c]){
-                    visited[r][c]=true;
-                    q.offer(new Pair(r, c, dist+1));
+        m = grid.length;
+        n = grid[0].length;
+        if (grid[0][0] != 0 || grid[m - 1][n - 1] != 0)
+            return -1;
+        return bfs(grid);
+    }
+
+    int bfs(int[][] grid) {
+        boolean[][] visited = new boolean[m][n];
+        Queue<int[]> q = new LinkedList<>();
+        visited[0][0] = true;
+        int t = 1;
+        q.offer(new int[] { 0, 0 });
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int p = 0; p < size; p++) {
+                int[] curr = q.poll();
+                int r = curr[0];
+                int c = curr[1];
+
+                if (r == m - 1 && c == n - 1) {
+                    return t;
+                }
+                for (int i = 0; i < 8; i++) {
+                    int nr = r + dr[i];
+                    int nc = c + dc[i];
+                    if (nr >= 0 && nc >= 0 && nr < m && nc < n && grid[nr][nc] == 0 && !visited[nr][nc]) {
+                        visited[nr][nc] = true;
+                        q.offer(new int[] { nr, nc});
+                    }
                 }
             }
+            t+=1;
         }
         return -1;
     }
