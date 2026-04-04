a59186aab4b7686c43624265f824633b61f43900
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-04 09:02:27 +0530

    0785-is-graph-bipartite .java

--- LeetCode/Medium/0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ LeetCode/Medium/0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,29 +1,33 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        int n=graph.length;
-        boolean[] visited=new boolean[n];
-        int color[]=new int[n];
-        
-        for(int l=0;l<n;l++){
-            if(!visited[l]){
-                color[l]=5;
-                Queue<Integer> q=new LinkedList<>();
-                q.offer(l);
-                while(!q.isEmpty()){
-                    int x=q.poll();
-                    int c=color[x];
-                    visited[x]=true;
-                    for(int i: graph[x]){
-                        if(color[i]==c)return false;
-                        if(!visited[i]){
-                            color[i]=c+1;
-                            q.offer(i);
+        int n = graph.length;
+        int[] color = new int[n];
+        Arrays.fill(color, -1);
+        for (int i = 0; i < n; i++) {
+            if (color[i] == -1) {
+                Queue<int[]> q = new LinkedList<>();
+                q.offer(new int[] { i, 1 });
+                color[i] = 1;
+                while (!q.isEmpty()) {
+                    int[] arr = q.poll();
+                    int u = arr[0];
+                    int c = arr[1];
+                    for (int v : graph[u]) {
+                        int nc = 0;
+                        if (c == 1)
+                            nc = 2;
+                        else
+                            nc = 1;
+                        if (color[v] == -1) {
+                            color[v] = nc;
+                            q.offer(new int[] { v, nc });
+                        } else if (color[v] != nc) {
+                            return false;
                         }
                     }
                 }
             }
         }
         return true;
-        
     }
 }
