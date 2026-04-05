414b3e48785b0945e968cbd258a04a9b4e60fe24
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-06 00:50:57 +0530

    0257-binary-tree-paths .java

---
+++ LeetCode/Easy/0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<String> ans;
+    public List<String> binaryTreePaths(TreeNode root) {
+        ans= new ArrayList<>();
+        dfs(root, new StringBuilder());
+        return ans;
+    }
+    void dfs(TreeNode root, StringBuilder sb){
+        
+        if(root==null)return;
+        if(root.left==null && root.right==null){
+            ans.add(sb.append(root.val).toString());
+            return;
+        }
+        int len=sb.length();
+        sb.append(root.val).append("->");
+        dfs(root.left, sb);
+        dfs(root.right, sb);
+        sb.setLength(len);
+    }
+}
