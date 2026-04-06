75cbd2bdac107b8c0c12276bfca57bc9563283be
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-06 08:56:23 +0530

    0987-vertical-order-traversal-of-a-binary-tree .java

--- LeetCode/Hard/0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ LeetCode/Hard/0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -14,51 +14,45 @@
  * }
  */
 class Solution {
-    class Pair {
-        int r;
-        int c;
-        TreeNode root;
-
-        Pair(int r, int c, TreeNode root) {
-            this.r = r;
-            this.c = c;
-            this.root = root;
+    class Pair{
+        TreeNode node;
+        int r, c;
+        Pair(TreeNode node, int r, int c){
+            this.node=node;
+            this.r=r;
+            this.c=c;
         }
     }
-
-    TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> tm;
-
+    TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> tm;//sort by col, row, value
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        tm = new TreeMap<>();
-        Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(0, 0, root));// r, c, node
+        tm=new TreeMap<>();
 
-        while (!q.isEmpty()) {
-            Pair p = q.poll();
-            TreeNode node = p.root;
-            int r = p.r;
-            int c = p.c;
+        Queue<Pair> q= new LinkedList<>();
+        q.offer(new Pair(root, 0, 0));
+
+        while(!q.isEmpty()){
+            Pair p=q.poll();
+            TreeNode node=p.node;
+            int r=p.r;
+            int c=p.c;
             tm.putIfAbsent(c, new TreeMap<>());
             tm.get(c).putIfAbsent(r, new PriorityQueue<>());
-            tm.get(c).get(r).add(node.val);
-
-            if (node.left != null) {
-                q.offer(new Pair(r + 1, c - 1, node.left));
+            tm.get(c).get(r).offer(node.val);
+            if(node.left!=null){
+                q.offer(new Pair(node.left, r+1, c-1));
             }
-            if (node.right != null) {
-                q.offer(new Pair(r + 1, c + 1, node.right));
+            if(node.right!=null){
+                q.offer(new Pair(node.right, r+1, c+1));
             }
         }
-        List<List<Integer>> ans = new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
 
-        for (TreeMap<Integer, PriorityQueue<Integer>> p : tm.values()) {
-            List<Integer> ls = new ArrayList<>();
-
-            for (PriorityQueue<Integer> pq : p.values()) {
-                while (!pq.isEmpty()) {
+        for(TreeMap<Integer, PriorityQueue<Integer>> v: tm.values()){
+            List<Integer> ls =  new ArrayList<>();
+            for(PriorityQueue<Integer> pq: v.values()){
+                while(!pq.isEmpty()){
                     ls.add(pq.poll());
                 }
-
             }
             ans.add(ls);
         }
