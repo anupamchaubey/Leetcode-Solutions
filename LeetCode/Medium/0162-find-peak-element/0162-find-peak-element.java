3e563e5320328e32c3ec1b8714fd73fe1a0698fa
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-09 10:39:15 +0530

    0162-find-peak-element .java

--- LeetCode/Medium/0162-find-peak-element/0162-find-peak-element.java
+++ LeetCode/Medium/0162-find-peak-element/0162-find-peak-element.java
@@ -1,19 +1,18 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int n=nums.length;
+        int n = nums.length;
         if(n==1)return 0;
         if(nums[0]>nums[1])return 0;
         if(nums[n-1]>nums[n-2])return n-1;
-        int l=1,r=n-2;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]>nums[mid-1] && nums[mid]> nums[mid+1]){
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (mid > 0 && mid < n && nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
                 return mid;
-            }
-            if(nums[mid]<nums[mid+1]){
-                l=mid+1;
-            }else{
-                r=mid-1;
+            } else if (mid < n && nums[mid] < nums[mid + 1]) {
+                l = mid + 1;
+            } else if (mid > 0 && nums[mid] < nums[mid - 1]) {
+                r = mid - 1;
             }
         }
         return -1;
