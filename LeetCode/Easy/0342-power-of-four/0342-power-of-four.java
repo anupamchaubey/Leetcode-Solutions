f30d01c71557d0745d96cbb6ea00abdcd98172b1
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-15 14:44:50 +0530

    0342-power-of-four .java

--- LeetCode/Easy/0342-power-of-four/0342-power-of-four.java
+++ LeetCode/Easy/0342-power-of-four/0342-power-of-four.java
@@ -1,9 +1,7 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n==1)return true;
-        if(n<4)return false;
         while(n>0){
-            if(n==1|| n==4)return true;
+            if(n==1 || n==4)return true;
             if(n<4 || n%4!=0)return false;
             n/=4;
         }
