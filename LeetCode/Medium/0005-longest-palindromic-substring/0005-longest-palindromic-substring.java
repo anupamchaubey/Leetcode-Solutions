e48500150db53ecf07ffad0cd05c52b8cc37866c
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-15 15:58:59 +0530

    0005-longest-palindromic-substring .java

--- LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,38 +1,24 @@
 class Solution {
     public String longestPalindrome(String s) {
-        boolean[][] pal=new boolean[s.length()][s.length()];
-        for(int i=0;i<s.length();i++){
-            for(int j=i;j<s.length();j++){
+        int max=0;
+        int st=0,en=0;
+        int n=s.length();
+        if(n==0)return "";
+        boolean[][] pal=new boolean[n][n];
+        for(int j=0;j<n;j++){
+            for(int i=0;i<=j;i++){
                 if(s.charAt(i)==s.charAt(j)){
                     if(j-i<=2 || pal[i+1][j-1]){
                         pal[i][j]=true;
-                    }
-                }
-            }
-        }
-        int max=0;
-        int st=0,en=0;
-        for(int i=0;i<s.length();i++){
-            for(int j=i;j<s.length();j++){
-                if(pal[i][j]){
-                    if(max<j-i+1){
-                        max=j-i+1;
-                        st=i;
-                        en=j;
+                        if(j-i+1>max){
+                            max=j-i+1;
+                            st=i;
+                            en=j;
+                        }
                     }
                 }
             }
         }
         return s.substring(st, en+1);
-    }
-    boolean isPal(String s, int i, int j){
-        while(i<j){
-            if(s.charAt(i)!=s.charAt(j)){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
     }
 }
