b0142267574a636308a8a57de94f1edcbff4ed8a
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-22 02:11:06 +0530

    0130-surrounded-regions .java

--- LeetCode/Medium/0130-surrounded-regions/0130-surrounded-regions.java
+++ LeetCode/Medium/0130-surrounded-regions/0130-surrounded-regions.java
@@ -8,52 +8,44 @@ class Solution {
 
         for(int i=0;i<m;i++){
             if(board[i][0]=='O'){
-                dfs(board, i, 0, true);
+                dfs(board, i, 0);
             }
         }
         for(int i=0;i<m;i++){
             if(board[i][n-1]=='O'){
-                dfs(board, i, n-1, true);
+                dfs(board, i, n-1);
             }
         }
         for(int i=0;i<n;i++){
             if(board[0][i]=='O'){
-                dfs(board, 0, i, true);
+                dfs(board, 0, i);
             }
         }
         for(int i=0;i<n;i++){
             if(board[m-1][i]=='O'){
-                dfs(board, m-1, i, true);
+                dfs(board, m-1, i);
             }
         }
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(board[i][j]=='O'){
-                    dfs(board, i, j, false);
-                }
-            }
-        }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
                 if(board[i][j]=='P'){
                     board[i][j]='O';
+                }else if(board[i][j]=='O'){
+                    board[i][j]='X';
                 }
             }
         }
     }
-    void dfs(char[][] grid, int r, int c, boolean flag){
+    void dfs(char[][] grid, int r, int c){
         if(r<0 || c<0 || r>=m || c>=n || grid[r][c]!='O')return;
-        
-        if(flag){
-            grid[r][c]='P';
-        }else{
-            grid[r][c]='X';
-        }
+
+        grid[r][c]='P';
+
         for(int i=0;i<4;i++){
             int nr=r+dr[i];
             int nc=c+dc[i];
-            dfs(grid, nr, nc, flag);
+            dfs(grid, nr, nc);
         }
     }
 
