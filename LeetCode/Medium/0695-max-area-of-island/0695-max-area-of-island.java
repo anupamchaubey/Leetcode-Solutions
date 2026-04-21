603943fcc4978f435f8a8f45209a1e61f897045d
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-22 01:47:53 +0530

    0695-max-area-of-island .java

--- LeetCode/Medium/0695-max-area-of-island/0695-max-area-of-island.java
+++ LeetCode/Medium/0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,32 +1,29 @@
 class Solution {
-    int m;
-    int n;
     int[] dr={-1, 1, 0, 0};
     int[] dc={0, 0, -1, 1};
+    int m, n;
     public int maxAreaOfIsland(int[][] grid) {
-        int max=0;
         m=grid.length;
         n=grid[0].length;
-        boolean[][] visited=new boolean[m][n];
+        int max=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==1 && !visited[i][j]){
-                    visited[i][j]=true;
-                    max=Math.max(max, dfs(grid, i, j, visited));
+                if(grid[i][j]==1){
+                    max=Math.max(max, dfs(grid, i, j));
                 }
             }
         }
         return max;
     }
-    int dfs(int[][] grid, int r, int c, boolean[][] visited){
-        int area=1;
+    int dfs(int[][] grid, int r, int c){
+        if(r<0 || c<0 || r>=m || c>=n || grid[r][c]!=1)return 0;
+        grid[r][c]=2;
+        int ans=1;
         for(int i=0;i<4;i++){
             int nr=r+dr[i];
             int nc=c+dc[i];
-            if(nr<0 || nc<0 || nr>=m || nc>=n || visited[nr][nc] || grid[nr][nc]!=1)continue;
-            visited[nr][nc]=true;
-            area+=dfs(grid, nr, nc, visited);
+            ans+=dfs(grid, nr, nc);
         }
-        return area;
+        return ans;
     }
 }
