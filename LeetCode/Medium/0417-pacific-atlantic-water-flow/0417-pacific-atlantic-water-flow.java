6aa9fcf297307d9333200e338ecf14a29caeea5c
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-22 02:41:15 +0530

    0417-pacific-atlantic-water-flow .java

--- LeetCode/Medium/0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ LeetCode/Medium/0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,54 +1,43 @@
 class Solution {
-    int[] dr = { -1, 1, 0, 0 };
-    int[] dc = { 0, 0, -1, 1 };
-    int m;
-    int n;
-
+    List<List<Integer>> ans;
+    int m,n;
+    int[] dr={-1, 1, 0, 0};
+    int[] dc={0, 0, -1, 1};
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        m = heights.length;
-        n = heights[0].length;
-        List<List<Integer>> ans = new ArrayList<>();
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                boolean[][] visited1 = new boolean[m][n];
-                boolean[][] visited2 = new boolean[m][n];
-                if (pac(heights, i, j, visited1) && atl(heights, i, j, visited2)) {
-                    List<Integer> ls = new ArrayList<>();
-                    ls.add(i);
-                    ls.add(j);
-                    ans.add(new ArrayList<>(ls));
+        m=heights.length;
+        n=heights[0].length;
+        ans=new ArrayList<>();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(pacific(heights, i, j, new boolean[m][n]) && atlantic(heights, i, j, new boolean[m][n])){
+                    ans.add(Arrays.asList(i, j));
                 }
             }
         }
         return ans;
     }
-
-    boolean pac(int[][] heights, int r, int c, boolean[][] visited) {
-        if (r == 0 || c == 0)
-            return true;
-        for (int i = 0; i < 4; i++) {
-            int nr = r + dr[i];
-            int nc = c + dc[i];
-            if (nr >= 0 && nc >= 0 && nr < m && nc < n && heights[nr][nc] <= heights[r][c] && !visited[nr][nc]) {
-                visited[nr][nc] = true;
-                if (pac(heights, nr, nc, visited))
-                    return true;
+    boolean pacific(int[][] grid, int r, int c, boolean[][] visited){
+        if(r==0 || c==0)return true;
+        visited[r][c]=true;
+        for(int i=0;i<4;i++){
+            int nr=r+dr[i];
+            int nc=c+dc[i];
+            if(nr>=0 && nc>=0 && nr<m && nc<n && !visited[nr][nc] && grid[nr][nc]<=grid[r][c]){
+                if(pacific(grid, nr, nc, visited))return true;
             }
         }
         return false;
     }
 
-    boolean atl(int[][] heights, int r, int c, boolean[][] visited) {
-        if (r == m - 1 || c == n - 1)
-            return true;
-        for (int i = 0; i < 4; i++) {
-            int nr = r + dr[i];
-            int nc = c + dc[i];
-            if (nr >= 0 && nc >= 0 && nr < m && nc < n && heights[nr][nc] <= heights[r][c] && !visited[nr][nc]) {
-                visited[nr][nc] = true;
-                if (atl(heights, nr, nc, visited))
-                    return true;
+
+    boolean atlantic(int[][] grid, int r, int c, boolean[][] visited){
+        if(r==m-1 || c==n-1)return true;
+        visited[r][c]=true;
+        for(int i=0;i<4;i++){
+            int nr=r+dr[i];
+            int nc=c+dc[i];
+            if(nr>=0 && nc>=0 && nr<m && nc<n && !visited[nr][nc] && grid[nr][nc]<=grid[r][c]){
+                if(atlantic(grid, nr, nc, visited))return true;
             }
         }
         return false;
