da8323c824b48865d51afa6fec05c6497b36012e
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-21 23:22:23 +0530

    0797-all-paths-from-source-to-target .java

--- LeetCode/Medium/0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ LeetCode/Medium/0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -1,23 +1,29 @@
 class Solution {
     List<List<Integer>> ans;
+
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        ans=new ArrayList<>();
-        dfs(graph, 0, new ArrayList<>(), graph.length-1);
+        ans = new ArrayList<>();
+        dfs(graph, 0, new ArrayList<>(), graph.length - 1);
         return ans;
     }
-    void dfs(int[][] graph, int node, List<Integer> ls,int dest){
-        if(node==dest){
-            ls.add(node);
-            ans.add(new ArrayList<>(ls));
-            ls.remove(ls.size()-1);
-            return;
-        }
+
+    void dfs(int[][] graph, int node, List<Integer> ls, int dest) {
+
         ls.add(node);
-        for(int v: graph[node]){
+
+        if (node == dest) {
+
+            ans.add(new ArrayList<>(ls));
             
-            dfs(graph, v, ls, dest);
+        } else {
             
+            for (int v : graph[node]) {
+
+                dfs(graph, v, ls, dest);
+
+            }
         }
-        ls.remove(ls.size()-1);
+
+        ls.remove(ls.size() - 1);
     }
 }
