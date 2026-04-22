272a2218c049a5d182f7ef03e10b600f6ecce540
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-22 19:46:57 +0530

    0494-target-sum .java

--- LeetCode/Medium/0494-target-sum/0494-target-sum.java
+++ LeetCode/Medium/0494-target-sum/0494-target-sum.java
@@ -1,18 +1,15 @@
 class Solution {
-
     public int findTargetSumWays(int[] nums, int target) {
-        return rec(nums, 0, target, 0);
+        return rec(nums, 0, target);
     }
-    int rec(int[] nums, int idx, int target, int sum){
-        if(idx>=nums.length){
-            if(sum==target){
-                return 1;
-            }
+    int rec(int[] nums, int idx, int target){
+        if(idx==nums.length){
+            if(target==0)return 1;
             return 0;
         }
-        int c=0;
-        c+=rec(nums, idx+1, target, sum+nums[idx]);
-        c+=rec(nums, idx+1, target, sum-nums[idx]);
-        return c;
+        int count=0;
+        count+=rec(nums, idx+1, target-nums[idx]);
+        count+=rec(nums, idx+1, target+nums[idx]);
+        return count;
     }
 }
