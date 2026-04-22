a377666ea77f366b0402af10738cb018930125dc
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-22 20:20:07 +0530

    0494-target-sum .java

--- LeetCode/Medium/0494-target-sum/0494-target-sum.java
+++ LeetCode/Medium/0494-target-sum/0494-target-sum.java
@@ -1,5 +1,7 @@
 class Solution {
+    HashMap<String, Integer> hm;
     public int findTargetSumWays(int[] nums, int target) {
+        hm=new HashMap<>();
         return rec(nums, 0, target);
     }
     int rec(int[] nums, int idx, int target){
@@ -7,9 +9,12 @@ class Solution {
             if(target==0)return 1;
             return 0;
         }
+        String key=idx+"-"+target;
+        if(hm.containsKey(key))return hm.get(key);
         int count=0;
         count+=rec(nums, idx+1, target-nums[idx]);
         count+=rec(nums, idx+1, target+nums[idx]);
+        hm.put(key, count);
         return count;
     }
 }
