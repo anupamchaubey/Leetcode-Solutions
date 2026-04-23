ada946a8dbba3870ec9d84c7bcbe0192ae3c22b1
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-24 03:33:18 +0530

    0918-maximum-sum-circular-subarray .java

--- LeetCode/Medium/0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
+++ LeetCode/Medium/0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
@@ -1,25 +1,28 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
-        int linearmin = Integer.MAX_VALUE;
-        int sum2 = 0;
-        int linearmax = Integer.MIN_VALUE;
-        int sum = 0;
-        int n = nums.length;
-        int total = 0;
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-            linearmax = Math.max(linearmax, sum);
-            if (sum < 0)
-                sum = 0;
-            sum2 += nums[i];
-            linearmin = Math.min(sum2, linearmin);
-            if (sum2 > 0)
-                sum2 = 0;
-            total += nums[i];
-        }
-        if (linearmax < 0)
-            return linearmax;
+        
+        int n=nums.length;
 
-        return Math.max(linearmax, total - linearmin);
+        //find the linear max
+        int max=Integer.MIN_VALUE;
+        int sum=0;
+        int total=0;
+
+        for(int i=0;i<n;i++){
+            total+=nums[i];
+            sum+=nums[i];
+            max=Math.max(max, sum);
+            if(sum<0)sum=0;
+        }
+        //find the minimum subarray to subtract from the circular array, which gives the right answer
+        int min=Integer.MAX_VALUE;
+        sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            min=Math.min(sum, min);
+            if(sum>0)sum=0;
+        }
+        if(total==min)return max;
+        return Math.max(max, total-min);
     }
 }
