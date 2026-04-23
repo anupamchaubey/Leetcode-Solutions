ab6bda8298d13bb6a2bb40c1ec91b8ff42516e3f
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-23 22:09:19 +0530

    0075-sort-colors .java

--- LeetCode/Medium/0075-sort-colors/0075-sort-colors.java
+++ LeetCode/Medium/0075-sort-colors/0075-sort-colors.java
@@ -1,22 +1,25 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int n=nums.length;
-        int st=0, mid=0, en=n-1;
-        while(mid<=en){
-            if(nums[mid]==0){
-                int temp=nums[st];
-                nums[st]=nums[mid];
-                nums[mid]=temp;
-                st++;
-                mid++;
-            }else if(nums[mid]==2){
-                int temp=nums[en];
-                nums[en]=nums[mid];
-                nums[mid]=temp;
-                en--;
+        // dutch national flag algorithm
+
+        int l=0, m=0, r=nums.length-1;
+
+        while(m<=r){
+            if(nums[m]==0){
+                int temp=nums[l];
+                nums[l]=nums[m];
+                nums[m]=temp;
+                l++;
+                m++;
+            }else if(nums[m]==1){
+                m++;
             }else{
-                mid++;
+                int temp=nums[m];
+                nums[m]=nums[r];
+                nums[r]=temp;
+                r--;
             }
         }
+        
     }
 }
