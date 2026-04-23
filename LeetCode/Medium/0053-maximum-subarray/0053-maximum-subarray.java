e8cf1464742707596570f9b8bbddd5ebecd289f3
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-24 02:42:20 +0530

    0053-maximum-subarray .java

--- LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.java
+++ LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,51 +1,14 @@
- class Solution {
-
-    public  int maxSubArray(int[] nums) {
-        return findMaxSum(nums, 0, nums.length - 1);
-    }
-
-    private  int findMaxSum(int[] nums, int left, int right) {
-        // Base Case: Only one element
-        if (left == right) {
-            return nums[left];
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=Integer.MIN_VALUE;
+        int sum=0;
+        int n=nums.length;
+        int l=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            max=Math.max(max, sum);
+            if(sum<0)sum=0;
         }
-
-        int mid = left + (right - left) / 2;
-
-        // Recursively find the max in the left and right halves
-        int leftMax = findMaxSum(nums, left, mid);
-        int rightMax = findMaxSum(nums, mid + 1, right);
-        
-        // Find the max sum that crosses the midpoint
-        int crossMax = findCrossingSum(nums, left, mid, right);
-
-        // Return the best of the three
-        return Math.max(Math.max(leftMax, rightMax), crossMax);
+        return max;
     }
-
-    private  int findCrossingSum(int[] nums, int left, int mid, int right) {
-        // Find max sum starting from mid and moving left
-        int sum = 0;
-        int leftPartMax = Integer.MIN_VALUE;
-        for (int i = mid; i >= left; i--) {
-            sum += nums[i];
-            if (sum > leftPartMax) {
-                leftPartMax = sum;
-            }
-        }
-
-        // Find max sum starting from mid + 1 and moving right
-        sum = 0;
-        int rightPartMax = Integer.MIN_VALUE;
-        for (int i = mid + 1; i <= right; i++) {
-            sum += nums[i];
-            if (sum > rightPartMax) {
-                rightPartMax = sum;
-            }
-        }
-
-        return leftPartMax + rightPartMax;
-    }
-
-    
 }
