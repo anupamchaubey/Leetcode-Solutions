89aae7a02156d29773ca809976aa9c49cb3efc21
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-24 22:00:56 +0530

    0904-fruit-into-baskets .java

--- LeetCode/Medium/0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ LeetCode/Medium/0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,22 +1,27 @@
 class Solution {
+    // pattern : atmost k distinct elements
     public int totalFruit(int[] fruits) {
-        return atmost(fruits, 2);
-    }
-    int atmost(int[] fruits, int k){
-        HashMap<Integer, Integer> hm=new HashMap<>();
-        int count=0;
-        int left=0;
-        int max=0;
-        for(int r=0;r<fruits.length;r++){
-            hm.put(fruits[r], hm.getOrDefault(fruits[r], 0)+1);
 
-            while(hm.size()>2){
-                hm.put(fruits[left], hm.get(fruits[left])-1);
-                if(hm.get(fruits[left])==0)hm.remove(fruits[left]);
+        return atmost(fruits, 2);
+
+    }
+    int atmost(int[] fruits, int k) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        int count = 0;
+        int left = 0;
+        int max = 0;
+        for (int r = 0; r < fruits.length; r++) {
+            hm.put(fruits[r], hm.getOrDefault(fruits[r], 0) + 1);
+
+            while (hm.size() > k) {
+                hm.put(fruits[left], hm.get(fruits[left]) - 1);
+                if (hm.get(fruits[left]) == 0)
+                    hm.remove(fruits[left]);
                 left++;
             }
-            count=r-left+1;
-            max=Math.max(max, count);
+            count = r - left + 1;
+            max = Math.max(max, count);
         }
         return max;
     }
