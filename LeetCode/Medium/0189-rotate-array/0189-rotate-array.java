7c0a47567b766c93181bb09a29d24e55f4022f30
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-25 02:25:59 +0530

    0189-rotate-array .java

---
+++ LeetCode/Medium/0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n=nums.length;
+        k=k%n;
+
+        int i=0, j=n-k-1;
+        while(i<j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
+        }
+        i=n-k;
+        j=n-1;
+        while(i<j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
+        }
+        i=0;
+        j=n-1;
+        while(i<j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
+        }
+        
+    }
+}
