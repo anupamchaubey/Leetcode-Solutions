4d26dc42a0cb1b13ae46ec5417c0a829e3f5bc32
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-24 12:12:32 +0530

    1631-path-with-minimum-effort .java

--- LeetCode/Medium/1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ LeetCode/Medium/1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,41 +1,41 @@
 class Solution {
-    public int minimumEffortPath(int[][] height) {
-        int[] dr={1, -1, 0, 0};
+    class Pair{
+        int r, c, eff;
+        Pair(int r, int c, int eff){
+            this.r=r;
+            this.c=c;
+            this.eff=eff;
+        }
+    }
+    public int minimumEffortPath(int[][] heights) {
+        int[] dr={-1, 1, 0, 0};
         int[] dc={0, 0, -1, 1};
-
-        int m=height.length;
-        int n=height[0].length;
-        int[][] dist=new int[m][n];
-
+        int[][] dist= new int[heights.length][heights[0].length];
         for(int[] arr: dist){
             Arrays.fill(arr, Integer.MAX_VALUE);
         }
         dist[0][0]=0;
 
-        PriorityQueue<int[]> q= new PriorityQueue<>((a, b)-> Integer.compare(a[2], b[2])); // i, j, diff 
+        PriorityQueue<Pair> pq= new PriorityQueue<>((a,b)-> Integer.compare(a.eff, b.eff));
+        pq.offer(new Pair(0, 0, 0));
 
-        q.offer(new int[] {0, 0, 0});
+        while(!pq.isEmpty()){
+            Pair p=pq.poll();
+            int r=p.r;
+            int c=p.c;
+            int eff=p.eff;
 
-        while(!q.isEmpty()){
-            int[] arr=q.poll();
-            int i=arr[0];
-            int j=arr[1];
-            int d=arr[2];
-            if(i==m-1 && j==n-1){
-                return dist[i][j];
-            }
-            for(int p=0;p<4;p++){
-                int nr=i+dr[p];
-                int nc=j+dc[p];
-                if(nr>=0 && nc>=0 && nr<m && nc<n){
-                    int newEff=Math.max(d, Math.abs(height[i][j]-height[nr][nc]));
-                    if(newEff<dist[nr][nc]){
-                        dist[nr][nc]=newEff;
-                        q.offer(new int[]{nr, nc, dist[nr][nc]});
-                    }
+            for(int i=0;i<4;i++){
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+                if(nr<0 || nc<0 || nr>=heights.length|| nc>=heights[0].length)continue;
+                int neff=Math.max(eff, Math.abs(heights[nr][nc]-heights[r][c]));
+                if(dist[nr][nc]>neff){
+                    dist[nr][nc]=neff;
+                    pq.offer(new Pair(nr, nc, neff));
                 }
             }
         }
-        return dist[m-1][n-1];
+        return dist[heights.length-1][heights[0].length-1];
     }
 }
