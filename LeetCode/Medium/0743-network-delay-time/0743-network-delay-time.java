5640c9ec006820ecae952b55852607f65f0815f9
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-24 11:57:42 +0530

    0743-network-delay-time .java

--- LeetCode/Medium/0743-network-delay-time/0743-network-delay-time.java
+++ LeetCode/Medium/0743-network-delay-time/0743-network-delay-time.java
@@ -1,53 +1,49 @@
 class Solution {
-    class Pair {
+    class Pair{
         int v;
         int t;
-
-        Pair(int v, int t) {
-            this.v = v;
-            this.t = t;
+        Pair(int v, int t){
+            this.v=v;
+            this.t=t;
         }
     }
-
     public int networkDelayTime(int[][] times, int n, int k) {
-        List<List<Pair>> adj = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
+        List<List<Pair>> adj=new ArrayList<>();
+        for(int i=0;i<=n;i++){
             adj.add(new ArrayList<>());
         }
-        for (int[] t : times) {
-            int u = t[0];
-            int v = t[1];
-            int ti = t[2];
-            adj.get(u).add(new Pair(v, ti));
+        for(int[] e:times){
+            int u=e[0];
+            int v=e[1];
+            int t=e[2];
+            adj.get(u).add(new Pair(v, t));
         }
-        int[] dist = new int[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE / 2);
-        dist[0] = -1;
-        dist[k] = 0;
+        int[] dist=new int[n+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[k]=0;
 
-        PriorityQueue<Pair> q = new PriorityQueue<>((a, b) -> Integer.compare(a.t, b.t));
-
-        q.offer(new Pair(k, 0));
-
-        while (!q.isEmpty()) {
-            Pair p = q.poll();
-            int u = p.v;
-            if(p.t>dist[u])continue;
-            for (Pair pv : adj.get(u)) {
-                int v = pv.v;
-                if (dist[u] + pv.t < dist[v]) {
-                    dist[v] = dist[u] + pv.t;
-                    q.offer(new Pair(v, dist[v]));
+        PriorityQueue<Pair> pq= new PriorityQueue<>((a, b)-> Integer.compare(a.t, b.t));
+        pq.offer(new Pair(k, 0));
+        while(!pq.isEmpty()){
+            Pair p=pq.poll();
+            int u=p.v;
+            int t=p.t;
+            if(t>dist[u])continue;
+            for(Pair pair: adj.get(u)){
+                int v=pair.v;
+                int ct=pair.t;
+                if(t+ct<dist[v]){
+                    dist[v]=t+ct;
+                    pq.offer(new Pair(v, dist[v]));
                 }
-
             }
         }
-        int max = 0;
-        for (int x : dist)
-            max = Math.max(max, x);
-        if (max == Integer.MAX_VALUE / 2)
-            return -1;
-
+        int max=0;
+        for(int i=1;i<=n;i++){
+            int x=dist[i];
+            if(x==Integer.MAX_VALUE)return -1;
+            max=Math.max(max, x);
+        }
         return max;
     }
 }
