e539ad0a3f5a5074d14ee9126582def4c14887b0
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-24 14:10:01 +0530

    0787-cheapest-flights-within-k-stops .java

--- LeetCode/Medium/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ LeetCode/Medium/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,60 +1,52 @@
 class Solution {
-    class Pair {
-        int flight;
-        int price;
-
-        Pair(int flight, int price) {
-            this.flight = flight;
-            this.price = price;
+    class Pair{
+        int c, pr;
+        Pair(int c, int pr){
+            this.c=c;
+            this.pr=pr;
         }
     }
-
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<List<Pair>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        List<List<Pair>> adj= new ArrayList<>();
+        for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-        for (int[] f : flights) {
-            int u = f[0];
-            int v = f[1];
-            int cost = f[2];
-            adj.get(u).add(new Pair(v, cost));
+        for(int[] f:flights){
+            int u=f[0];
+            int v=f[1];
+            int pr=f[2];
+            adj.get(u).add(new Pair(v, pr));
         }
-
-        int[][] dist = new int[n][k + 2];
-        for (int[] d : dist) {
-            Arrays.fill(d, Integer.MAX_VALUE);
+        int[][] dist= new int[n+1][k+2];
+        for(int[] arr: dist){
+            Arrays.fill(arr, Integer.MAX_VALUE);
         }
-        dist[src][0] = 0;
+        dist[src][0]=0;
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]); // dst, price, steps
+        PriorityQueue<int[]> pq= new PriorityQueue<>((a, b)-> Integer.compare(a[1], b[1]));
+        pq.offer(new int[]{src, 0, 0}); // city, price, stops
 
-        pq.offer(new int[] { src, 0, 0 });
-
-        while (!pq.isEmpty()) {
-            int[] arr = pq.poll();
-            int u = arr[0];
-            int priceTillNow = arr[1];
-            int stepsTillNow = arr[2];
-
-            if (stepsTillNow > k)
-                continue;
-
-            for (Pair p : adj.get(u)) {
-                int v = p.flight;
-                int cost = p.price; // cost to destination v from u
-
-                if (dist[v][stepsTillNow + 1] > dist[u][stepsTillNow] + cost) {
-                    dist[v][stepsTillNow + 1] = dist[u][stepsTillNow] + cost;
-                    pq.offer(new int[] { v, dist[v][stepsTillNow + 1], stepsTillNow + 1 });
+        while(!pq.isEmpty()){
+            int[] arr= pq.poll();
+            int u=arr[0];
+            int pr=arr[1];
+            int st=arr[2];
+            if(st>k)continue;
+            for(Pair p: adj.get(u)){
+                int v=p.c;
+                int npr=pr+p.pr;
+                if(npr<dist[v][st+1]){
+                    dist[v][st+1]=npr;
+                    pq.offer(new int[]{v, dist[v][st+1], st+1});
                 }
             }
         }
         int min=Integer.MAX_VALUE;
         for(int i=0;i<=k+1;i++){
-            min=Math.min(min, dist[dst][i]);
+            min=Math.min(min, dist[dst][k+1]);
         }
         if(min==Integer.MAX_VALUE)return -1;
         return min;
+
     }
 }
