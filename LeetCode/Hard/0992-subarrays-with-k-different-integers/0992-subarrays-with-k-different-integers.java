b03a09eb95789ebe55c01ddb1f113a8f71018fc0
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-24 23:16:44 +0530

    0992-subarrays-with-k-different-integers .java

--- LeetCode/Hard/0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ LeetCode/Hard/0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,21 +1,21 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
+        // exactly(k) = atmost(nums, k) - atmost(nums, k-1)
         return atmost(nums, k)-atmost(nums, k-1);
     }
-    // exactly(k) = atmost(nums, k) - atmost(nums, k-1)
     int atmost(int[] nums, int k){
+        if(k<0)return 0;
         HashMap<Integer, Integer> hm=new HashMap<>();
         int l=0;
         int count=0;
         for(int i=0;i<nums.length;i++){
             hm.put(nums[i], hm.getOrDefault(nums[i], 0)+1);
             while(hm.size()>k){
-                int x=nums[l];
-                hm.put(x, hm.get(x)-1);
-                if(hm.get(x)==0)hm.remove(x);
+                hm.put(nums[l], hm.get(nums[l])-1);
+                if(hm.get(nums[l])==0)hm.remove(nums[l]);
                 l++;
             }
-            count+=i-l+1;
+            count+=(i-l+1);
         }
         return count;
     }
