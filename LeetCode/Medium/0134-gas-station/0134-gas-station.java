3b88b2327ef866a725ef4e247b79de918ce82a11
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-25 22:42:06 +0530

    0134-gas-station .java

--- LeetCode/Medium/0134-gas-station/0134-gas-station.java
+++ LeetCode/Medium/0134-gas-station/0134-gas-station.java
@@ -1,21 +1,24 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int total = 0, totalcost = 0;
+
         int totalgas = 0;
+        int totalcost = 0;
+        for (int i = 0; i < gas.length; i++) {
+            totalgas += gas[i];
+            totalcost += cost[i];
+        }
+        if (totalgas < totalcost)
+            return -1;
+
+        int gases = 0;
         int start = 0;
         for (int i = 0; i < gas.length; i++) {
-            total += gas[i];
-            totalcost += cost[i];
-            totalgas += gas[i];
-            if (totalgas < cost[i]) {
+            gases += (gas[i] - cost[i]);
+            if (gases < 0) {
+                gases = 0;
                 start = i + 1;
-                totalgas = 0;
-            } else {
-                totalgas -= cost[i];
             }
         }
-        if (total < totalcost)
-            return -1;
         return start;
     }
 }
