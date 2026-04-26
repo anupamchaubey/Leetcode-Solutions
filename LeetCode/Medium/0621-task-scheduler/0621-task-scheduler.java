acf48acdb5a3b26a573f0ce53286c1e5b4052c96
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-26 16:13:58 +0530

    0621-task-scheduler .java

--- LeetCode/Medium/0621-task-scheduler/0621-task-scheduler.java
+++ LeetCode/Medium/0621-task-scheduler/0621-task-scheduler.java
@@ -1,34 +1,53 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         int[] freq = new int[26];
-        for (char ch : tasks)
+        for (char ch : tasks) {
             freq[ch - 'A']++;
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
-        for (int f : freq) {
-            if (f > 0)
-                pq.offer(f);
         }
-        int time = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            if (a[1] != b[1])
+                return Integer.compare(a[1], b[1]);
+            else
+                return Integer.compare(freq[b[0]], freq[a[0]]);
+        });
 
-        while (!pq.isEmpty()) {
-            int cycle = n + 1;
-            List<Integer> ls = new ArrayList<>();
-            while (cycle > 0 && !pq.isEmpty()) {
-                int x = pq.poll();
-                if (x - 1 > 0)
-                    ls.add(x - 1);
-                time++;
-                cycle--;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                pq.offer(new int[] { i, 0 });
             }
-            for (int x : ls)
-                pq.offer(x);
-
-            if (pq.isEmpty())
-                break;
-            time += cycle;
         }
-        return time;
+        int totalTasks = tasks.length;
+        int count = 0;
+        List<int[]> ls = new ArrayList<>();
 
+        while (totalTasks > 0) {
+            count++;
+            int[] p = pq.poll();
+
+            if (p[1] == 0) {
+                freq[p[0]]--;
+                totalTasks--;
+
+                if (freq[p[0]] > 0) {
+                    p[1] = n + 1;
+                } else {
+                    p[1] = Integer.MAX_VALUE;
+                }
+            }
+            ls.add(p);
+            while (!pq.isEmpty())
+                ls.add(pq.poll());
+
+            for (int[] task : ls) {
+                if (task[1] > 0 && task[1] != Integer.MAX_VALUE) {
+                    task[1]--;
+                }
+                if (task[1] != Integer.MAX_VALUE || freq[task[0]] > 0) {
+                    pq.offer(task);
+                }
+            }
+            ls.clear();
+        }
+        return count;
     }
 }
