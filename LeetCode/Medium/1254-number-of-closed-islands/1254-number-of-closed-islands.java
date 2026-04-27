bdfe9d072c62aee3f590cd58282ae25f994214d4
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-28 01:50:14 +0530

    1254-number-of-closed-islands .java

--- LeetCode/Medium/1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ LeetCode/Medium/1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -1,41 +1,70 @@
 class Solution {
-    int rows;
-    int cols;
-
     int[] dr={-1, 1, 0, 0};
-    int[] dc={0, 0, -1,1 };
+    int[] dc={0, 0, -1, 1};
+    int m, n;
     public int closedIsland(int[][] grid) {
-        rows = grid.length;
-        cols = grid[0].length;
-        int count = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == 0) {
-                    if (dfs(grid, i, j)) {
-                        count++;
-                    }
+        m=grid.length;
+        n=grid[0].length;
+        for(int i=0;i<m;i++){
+            if(grid[i][0]==0){
+                bfs(grid, i, 0);
+            }
+            if(grid[i][n-1]==0){
+                bfs(grid, i, n-1);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(grid[0][i]==0){
+                bfs(grid, 0, i);
+            }
+            if(grid[m-1][i]==0){
+                bfs(grid, m-1, i);
+            }
+        }
+        int count=0;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==0){
+                    if(dfs(grid, i, j))count++;
                 }
             }
         }
         return count;
 
+
+
     }
-
-    boolean dfs(int[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= rows || j >= cols)
-            return false;
-
-        if (grid[i][j] == 1)
-            return true;
-
-        grid[i][j] = 1;
-        boolean closed=true;
-        for(int p=0;p<4;p++){
-            int r=i+dr[p];
-            int c=j+dc[p];
-            closed&=dfs(grid, r, c);
+    boolean dfs(int[][] grid, int r, int c){
+        if(grid[r][c]==1)return true;
+        grid[r][c]=2;
+        boolean ans=true;
+        for(int i=0;i<4;i++){
+            int nr=r+dr[i];
+            int nc=c+dc[i];
+            
+            if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==0 || grid[nr][nc]==1){
+                ans=ans & dfs(grid, nr, nc);
+            }
         }
-        return closed;
-
+        return ans;
+    }
+    void bfs(int[][] grid, int r, int c){
+        Queue<int[]> q= new LinkedList<>();
+        q.offer(new int[]{r, c});
+        grid[r][c]=2;
+        while(!q.isEmpty()){
+            int[] p=q.poll();
+            r=p[0];
+            c=p[1];
+            for(int i=0;i<4;i++){
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+                if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==0){
+                    grid[nr][nc]=2;
+                    q.offer(new int[]{nr, nc});
+                }
+            }
+        }
     }
 }
