a0c5fa837653b7da53b62319972aaae894142b23
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-28 01:35:21 +0530

    0417-pacific-atlantic-water-flow .java

--- LeetCode/Medium/0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ LeetCode/Medium/0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,43 +1,65 @@
 class Solution {
-    int[] dr={-1, 1, 0, 0};
-    int[] dc={0, 0, -1, 1};
+    int[] dr = { -1, 1, 0, 0 };
+    int[] dc = { 0, 0, -1, 1 };
     int m;
     int n;
-    
+
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        m=heights.length;
-        n=heights[0].length;
-        List<List<Integer>> ans= new ArrayList<>();
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(pacific(heights, i, j, new boolean[m][n]) && atlantic(heights, i, j, new boolean[m][n])){
+        m = heights.length;
+        n = heights[0].length;
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacific(heights, i, j) && atlantic(heights, i, j)) {
                     ans.add(Arrays.asList(i, j));
                 }
             }
         }
         return ans;
     }
-    boolean pacific(int[][] heights, int r, int c, boolean[][] visited){
-        if(r==0 || c==0)return true;
+
+    boolean pacific(int[][] heights, int r, int c) {
+        boolean[][] visited = new boolean[m][n];
+
+        Queue<int[]> q = new LinkedList<>();
         visited[r][c]=true;
-        for(int i=0;i<4;i++){
-            int nr=r+dr[i];
-            int nc=c+dc[i];
-            if(nr>=0 && nc>=0 && nr<m && nc<n && !visited[nr][nc] && heights[nr][nc]<=heights[r][c]){
-                if(pacific(heights, nr, nc, visited))return true;
+        q.offer(new int[] { r, c });
+        while (!q.isEmpty()) {
+            int[] p = q.poll();
+            if (p[0] == 0 || p[1] == 0)
+                return true;
+            for (int i = 0; i < 4; i++) {
+                int nr = p[0] + dr[i];
+                int nc = p[1] + dc[i];
+                if (nr >= 0 && nc >= 0 && nr < m && nc < n && !visited[nr][nc] && heights[nr][nc] <= heights[p[0]][p[1]]) {
+                    visited[nr][nc]=true;
+                    q.offer(new int[]{nr, nc});
+                }
             }
         }
         return false;
+
+        
     }
-    boolean atlantic(int[][] heights, int r, int c, boolean[][] visited){
-        if(r==m-1 || c==n-1)return true;
+
+    boolean atlantic(int[][] heights, int r, int c) {
+        boolean[][] visited = new boolean[m][n];
+
+        Queue<int[]> q = new LinkedList<>();
         visited[r][c]=true;
-        for(int i=0;i<4;i++){
-            int nr=r+dr[i];
-            int nc=c+dc[i];
-            if(nr>=0 && nc>=0 && nr<m && nc<n && !visited[nr][nc] && heights[nr][nc]<=heights[r][c]){
-                if(atlantic(heights, nr, nc, visited))return true;
+        q.offer(new int[] { r, c });
+        while (!q.isEmpty()) {
+            int[] p = q.poll();
+            if (p[0] == m-1 || p[1] == n-1)
+                return true;
+            for (int i = 0; i < 4; i++) {
+                int nr = p[0] + dr[i];
+                int nc = p[1] + dc[i];
+                if (nr >= 0 && nc >= 0 && nr < m && nc < n && !visited[nr][nc] && heights[nr][nc] <= heights[p[0]][p[1]]) {
+                    visited[nr][nc]=true;
+                    q.offer(new int[]{nr, nc});
+                }
             }
         }
         return false;
