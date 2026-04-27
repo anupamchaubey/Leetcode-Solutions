171a5bc01c9fbafd3e9f1aaea3c14c633ddaa8a0
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-28 02:07:06 +0530

    0494-target-sum .java

--- LeetCode/Medium/0494-target-sum/0494-target-sum.java
+++ LeetCode/Medium/0494-target-sum/0494-target-sum.java
@@ -1,20 +1,17 @@
 class Solution {
-    HashMap<String, Integer> hm;
     public int findTargetSumWays(int[] nums, int target) {
-        hm=new HashMap<>();
-        return rec(nums, 0, target);
+        
+
+        return ways(nums, 0, target);
     }
-    int rec(int[] nums, int idx, int target){
-        if(idx==nums.length){
+    int ways(int[] nums, int idx, int target){
+        if(idx==nums.length ){
             if(target==0)return 1;
             return 0;
         }
-        String key=idx+"-"+target;
-        if(hm.containsKey(key))return hm.get(key);
         int count=0;
-        count+=rec(nums, idx+1, target-nums[idx]);
-        count+=rec(nums, idx+1, target+nums[idx]);
-        hm.put(key, count);
+        count+=ways(nums, idx+1, target-nums[idx]);
+        count+=ways(nums, idx+1, target+nums[idx]);
         return count;
     }
 }
