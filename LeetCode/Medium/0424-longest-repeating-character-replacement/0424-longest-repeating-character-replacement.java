ddb05aaa29d46ce0608d586fc78777734d92620b
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-04-28 12:00:46 +0530

    0424-longest-repeating-character-replacement .java

--- LeetCode/Medium/0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ LeetCode/Medium/0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,23 +1,30 @@
 class Solution {
-    public int characterReplacement(String s, int k) {
-        return atmost(s, k);
-    }
-    int atmost(String s, int k){
-        int[] freq=new int[26];
-        int maxfreq=0;
-        int max=0;
-        int left=0;
-        for(int i=0;i<s.length();i++){
-            freq[s.charAt(i)-'A']++;
-            maxfreq=Math.max(freq[s.charAt(i)-'A'], maxfreq);
 
-            while((i-left+1)-maxfreq>k){
-                freq[s.charAt(left)-'A']--;
-                left++;
+    // look there written atmost k times
+    // remember pattern atmost k distinct ?
+
+    public int characterReplacement(String s, int k) {
+        return atmostk(s, k);
+    }
+    int atmostk(String s, int k){
+        
+        int[] freq= new int[128];
+        int ans=0;
+        int l=0;
+        int max=0;
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)]++;
+            max=Math.max(max, freq[s.charAt(i)]);
+
+            while(i-l+1-max>k){
+                
+                
+                freq[s.charAt(l)]--;
+                l++;
             }
-            for(int x: freq)maxfreq=Math.max(maxfreq, x);
-            max=Math.max(max, i-left+1);
+            ans=Math.max(ans, i-l+1);
         }
-        return max;
+        return ans;
+        
     }
 }
