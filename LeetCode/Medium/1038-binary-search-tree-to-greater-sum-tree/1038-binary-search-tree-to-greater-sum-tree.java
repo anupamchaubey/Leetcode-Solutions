6f79c6950cb95dc9e4a799156e83ac86fd0dbaf6
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-04 00:56:56 +0530

    1038-binary-search-tree-to-greater-sum-tree .java

--- LeetCode/Medium/1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
+++ LeetCode/Medium/1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
@@ -14,45 +14,17 @@
  * }
  */
 class Solution {
-    List<Integer> ls;
-    int prefix[];
-    HashMap<Integer, Integer> hm;
+    int sum;
     public TreeNode bstToGst(TreeNode root) {
-        ls=new ArrayList<>();
-        dfs(root);
-        prefix=new int[ls.size()];
-        prefix[ls.size()-1]=ls.get(ls.size()-1);
-
-        for(int i=ls.size()-2;i>=0;i--){
-            prefix[i]=prefix[i+1]+ls.get(i);
-        }
-        hm=new HashMap<>();
-        for(int i=0;i<ls.size();i++){
-            hm.put(ls.get(i), prefix[i]);
-        }
-        TreeNode root1=new TreeNode(hm.get(root.val));
-        dfs(root, root1);
-        return root1;
+        sum=0;
+        reverseInorder(root);
+        return root;
     }
-    
-    void dfs(TreeNode root, TreeNode root1){
+    void reverseInorder(TreeNode root){// gives descending order
         if(root==null)return;
-
-        if(root.left!=null){
-            int x=hm.get(root.left.val);
-            root1.left=new TreeNode(x);
-            dfs(root.left, root1.left);
-        }
-        if(root.right!=null){
-            int x=hm.get(root.right.val);
-            root1.right=new TreeNode(x);
-            dfs(root.right, root1.right);
-        }
-    }
-    void dfs(TreeNode root){
-        if(root==null)return;
-        dfs(root.left);
-        ls.add(root.val);
-        dfs(root.right);
+        reverseInorder(root.right);
+        sum+=root.val;
+        root.val=sum;
+        reverseInorder(root.left);
     }
 }
