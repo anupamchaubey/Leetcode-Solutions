c2e8705b329e458704aea04e1dcf3ead9a8463e4
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-03 22:42:38 +0530

    0111-minimum-depth-of-binary-tree .java

--- LeetCode/Easy/0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ LeetCode/Easy/0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -16,16 +16,15 @@
 class Solution {
     public int minDepth(TreeNode root) {
         if(root==null)return 0;
-        int ans=helper(root);
-        return ans;
+        return height(root);
     }
-
-    int helper(TreeNode root) {
-        if (root == null)
-            return Integer.MAX_VALUE;
-        if (root.left == null && root.right == null)
+    int height(TreeNode root){
+        if(root==null)return Integer.MAX_VALUE/3;
+        if(root.left==null && root.right==null){
             return 1;
-
-        return 1 + Math.min(helper(root.left), helper(root.right));
+        }
+        int left=height(root.left);
+        int right=height(root.right);
+        return 1+Math.min(left, right);
     }
 }
