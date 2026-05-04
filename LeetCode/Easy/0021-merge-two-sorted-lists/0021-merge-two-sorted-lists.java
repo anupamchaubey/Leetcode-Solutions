148db9cdf1c56bbab4dba3d0d318b01bf651d3ac
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-04 18:19:16 +0530

    0021-merge-two-sorted-lists .java

--- LeetCode/Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ LeetCode/Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -11,33 +11,28 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if(list1==null)return list2;
-        if(list2==null)return list1;
-        ListNode root=null;
+        else if(list2==null)return list1;
+        ListNode curr=null;
+        ListNode head=null;
         if(list1.val<list2.val){
-            root=list1;
-            list1 = list1.next;
-        }else{
-            root=list2;
-            list2 = list2.next;
+            curr=list1;
+            list1=list1.next;
+        }else {
+            curr=list2;
+            list2=list2.next;
         }
-        ListNode temp=root;
-        while(list1!=null && list2!=null){
-            if(list1.val<list2.val){
-                temp.next=list1;
-                list1=list1.next;
+        head=curr;
 
-            }else{
-                temp.next=list2;
-                list2=list2.next;
-                
+        while(curr!=null){
+            if(list2==null || (list1!=null && list1.val<list2.val)){
+                curr.next=list1;
+                if(list1!=null)list1=list1.next;
+            }else if(list1==null || (list2 != null && list2.val<=list1.val)){
+                curr.next=list2;
+                if(list2!=null)list2=list2.next;
             }
-            temp=temp.next;
-            
+            curr=curr.next;
         }
-        temp.next = (list1 != null) ? list1 : list2;
-        return root;
-
-
-
+        return head;
     }
 }
