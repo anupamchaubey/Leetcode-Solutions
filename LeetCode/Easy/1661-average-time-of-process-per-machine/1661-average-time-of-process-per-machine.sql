842dbb5a5b4c0aecc5592661d1d9ce5a9d92ab20
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-05 19:22:17 +0530

    1661-average-time-of-process-per-machine .sql

--- LeetCode/Easy/1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ LeetCode/Easy/1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,12 +1,10 @@
 # Write your MySQL query statement below
-
-Select
-a1.machine_id, 
-round(avg(a2.timestamp-a1.timestamp), 3) as processing_time
-from activity a1
-JOIN activity a2
-on a1.machine_id=a2.machine_id
-And a1.process_id=a2.process_id
-And a1.activity_type='start'
-And a2.activity_type='end'
-Group By a1.machine_id
+ 
+select a.machine_id,
+Round(AVG(b.timestamp-a.timestamp), 3) as processing_time
+From Activity a
+JOIN Activity b
+ON a.machine_id = b.machine_id
+and a.process_id=b.process_id
+where a.activity_type='start' and b.activity_type='end'
+Group By machine_id
