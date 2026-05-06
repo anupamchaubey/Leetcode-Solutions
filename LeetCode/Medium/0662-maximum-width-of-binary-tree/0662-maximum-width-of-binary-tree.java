1027b321ba41fcecd7a517098318be71223018f1
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-06 10:28:41 +0530

    0662-maximum-width-of-binary-tree .java

--- LeetCode/Medium/0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ LeetCode/Medium/0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -16,36 +16,34 @@
 class Solution {
     class Pair{
         TreeNode node;
-        int idx;
+        int idx;// index of the node in the array
         Pair(TreeNode node, int idx){
             this.node=node;
             this.idx=idx;
         }
     }
     public int widthOfBinaryTree(TreeNode root) {
-        int max=0;
-        Queue<Pair> q= new LinkedList<>();
+        // tree array me represent krne vali property use hui h
+        Queue<Pair> q=new LinkedList<>();
+        int maxwidth=0;
         q.offer(new Pair(root, 0));
         while(!q.isEmpty()){
-            int st=-1, en=-1;
             int size=q.size();
+            int st=-1, en=-1;
             for(int i=0;i<size;i++){
                 Pair p=q.poll();
                 TreeNode node=p.node;
-                int idx=p.idx;
-                if(st==-1){
-                    st=idx;
-                }
-                en=idx;
+                if(st==-1)st=p.idx;
                 if(node.left!=null){
-                    q.offer(new Pair(node.left, 2*idx+1));
+                    q.offer(new Pair(node.left, 2*p.idx+1));
                 }
                 if(node.right!=null){
-                    q.offer(new Pair(node.right, 2*idx+2));
+                    q.offer(new Pair(node.right, 2*p.idx+2));
                 }
+                en=p.idx;
             }
-            max=Math.max(max,en-st+1);
+            maxwidth=Math.max(maxwidth, en-st+1);
         }
-        return max;
+        return maxwidth;
     }
 }
