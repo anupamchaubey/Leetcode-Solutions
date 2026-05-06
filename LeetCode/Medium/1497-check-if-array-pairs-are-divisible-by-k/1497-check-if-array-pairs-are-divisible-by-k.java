e757b8b5b3254d94a89cf96ec7f8ab44b73700aa
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-06 11:12:37 +0530

    1497-check-if-array-pairs-are-divisible-by-k .java

---
+++ LeetCode/Medium/1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canArrange(int[] arr, int k) {
+        int n=arr.length;
+        if(n%2!=0)return false;
+
+        int[] freq=new int[k];
+
+        for(int i=0;i<n;i++){
+            int div=((arr[i]%k)+k)%k;
+            freq[div]++;
+        }
+
+        int pair=0;
+        for(int i=0;i<n;i++){
+            int rem=((arr[i]%k)+k)%k;
+            int need=((k-rem)%k);
+            if(freq[rem]!0){
+                pair++;
+                freq[rem]--;
+                freq[need]--;
+            }
+        }
+        return pair==n/2;
+    }
+}
