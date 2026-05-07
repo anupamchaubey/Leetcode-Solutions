38b848d6d260a6a7d51e69e4e96300052f57e12a
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-07 06:14:14 +0530

    0334-increasing-triplet-subsequence .java

--- LeetCode/Medium/0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ LeetCode/Medium/0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,16 +1,18 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        if(nums.length<3)return false;
-        int first=Integer.MAX_VALUE, second=Integer.MAX_VALUE;
-        for(int x:nums){
-            if(x<=first){
-                first=x;
-            }else if(x<=second){
-                second=x;
+        
+        int f=Integer.MAX_VALUE, s=Integer.MAX_VALUE;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<=f){
+                f=nums[i];
+            }else if(nums[i]<=s){
+                s=nums[i];
             }else{
                 return true;
             }
-    }
+        }
+
         return false;
     }
 }
