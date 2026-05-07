a2917058815589cf2fc644051cd075747bc840a1
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-07 07:42:25 +0530

    0767-reorganize-string .java

--- LeetCode/Medium/0767-reorganize-string/0767-reorganize-string.java
+++ LeetCode/Medium/0767-reorganize-string/0767-reorganize-string.java
@@ -1,48 +1,38 @@
 class Solution {
-    class Pair {
-        char ch;
-        int freq;
-
-        Pair(char ch, int freq) {
-            this.ch = ch;
-            this.freq = freq;
-        }
-    }
-
+    
     public String reorganizeString(String s) {
-        int n = 1;
-        int[] freq = new int[26];
-        for (char ch : s.toCharArray())
-            freq[ch - 'a']++;
+        int[] freq=new int[26];
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+        }
+        PriorityQueue<Character> pq=new PriorityQueue<>((a, b)-> freq[b-'a']-freq[a-'a']);
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> b.freq - a.freq);
-
-        for (int i = 0; i < 26; i++) {
-            int f = freq[i];
-            if (f > 0) {
-                pq.offer(new Pair((char) (i + 'a'), f));
+        for(int i=0;i<26;i++){
+            if(freq[i]>0){
+                pq.offer((char)('a'+i));
             }
         }
-        StringBuilder sb = new StringBuilder();
-        while (pq.size()>=2) {
-            
-            Pair p1 = pq.poll();
-            Pair p2= pq.poll();
-            
-            sb.append(p1.ch);
-            sb.append(p2.ch);
 
-            if(--p1.freq > 0 )pq.offer(p1);
-            if(--p2.freq > 0 )pq.offer(p2);
+        StringBuilder sb=new StringBuilder();
 
+        while(pq.size()>=2){
+            char ch1=pq.poll();
+            char ch2=pq.poll();
 
+            sb.append(ch1).append(ch2);
+
+            if(--freq[ch1-'a']>0){
+                pq.offer(ch1);
+            }
+            if(--freq[ch2-'a']>0){
+                pq.offer(ch2);
+            }
         }
-        if(!pq.isEmpty()){
-            Pair p= pq.poll();
-            sb.append(p.ch);
-            if(--p.freq>0)return "";
+        while(!pq.isEmpty()){
+            char ch=pq.poll();
+            sb.append(ch);
+            if(--freq[ch-'a']>0)return "";
         }
-            
         return sb.toString();
     }
 }
