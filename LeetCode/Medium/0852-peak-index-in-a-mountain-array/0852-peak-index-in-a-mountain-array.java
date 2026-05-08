02b1efad1163b910385a4c831cd3cbe22aef1be2
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-08 12:48:51 +0530

    0852-peak-index-in-a-mountain-array .java

--- LeetCode/Medium/0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ LeetCode/Medium/0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,21 +1,17 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int n=arr.length;
-        if(arr[0]>arr[1])return 0;
-        if(arr[n-1]>arr[n-2])return n-1;
+        int l = 0, r = arr.length - 1;
 
-        int l=0, r=arr.length-1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
 
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(mid<arr.length-1 && arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]){
-                return mid;
-            }else if(mid>0 && arr[mid]<=arr[mid-1]){
-                r=mid-1;
-            }else{
-                l=mid+1;
+            if (arr[mid] < arr[mid + 1]) {
+                l = mid + 1;
+            } else {
+                r = mid;
             }
         }
-        return n;
+
+        return l;
     }
 }
