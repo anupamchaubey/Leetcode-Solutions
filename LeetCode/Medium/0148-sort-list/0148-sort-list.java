2fb4aa16b2a3fd04621eb2436703e8a96372dcfb
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-10 20:17:52 +0530

    0148-sort-list .java

--- LeetCode/Medium/0148-sort-list/0148-sort-list.java
+++ LeetCode/Medium/0148-sort-list/0148-sort-list.java
@@ -10,20 +10,25 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
 
-        ListNode temp=head;
-        while(temp!=null){
-            pq.offer(temp.val);
-            temp=temp.next;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        ListNode temp = head;
+        while (temp != null) {
+            pq.offer(temp);
+            temp = temp.next;
         }
-        if(pq.isEmpty())return null;
-        ListNode node=new ListNode (pq.poll());
-        head=node;
-        while(!pq.isEmpty()){
-            node.next=new ListNode(pq.poll());
-            node=node.next;
+        if (pq.isEmpty())
+            return head;
+
+        head = pq.poll();
+        temp = head;
+        while (!pq.isEmpty()) {
+            temp.next = pq.poll();
+            temp = temp.next;
         }
+        temp.next = null;
+
         return head;
     }
 }
