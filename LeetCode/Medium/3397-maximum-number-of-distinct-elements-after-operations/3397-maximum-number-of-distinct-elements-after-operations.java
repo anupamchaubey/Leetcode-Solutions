9e109bb97a270e3ac270184d70efdf56d47b71c6
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-12 01:18:24 +0530

    3397-maximum-number-of-distinct-elements-after-operations .java

--- LeetCode/Medium/3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.java
+++ LeetCode/Medium/3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.java
@@ -1,20 +1,20 @@
 class Solution {
     public int maxDistinctElements(int[] nums, int k) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        int prev = Integer.MIN_VALUE;
-        int distinct = 0;
+        
+        Arrays.sort( nums);
+        int count=1;
+        int curr=nums[0]-k;
+        for(int i=1;i<nums.length;i++){
+            int st=nums[i]-k;
+            int en=nums[i]+k;
+            
+            int val=Math.max(curr+1, st);
 
-        for (int i = 0; i < n; i++) {
-            int start = nums[i] - k;
-            int end = nums[i] + k;
-            int consume = Math.max(prev + 1, start);
-
-            if (consume <= end) {
-                distinct++;
-                prev = consume;
+            if(val<=en){
+                curr=val;
+                count++;
             }
         }
-        return distinct;
+        return count;
     }
 }
