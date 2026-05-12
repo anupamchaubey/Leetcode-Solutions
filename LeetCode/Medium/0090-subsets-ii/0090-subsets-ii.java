4a41c79c8216aaa9faf3df399fe7c18f0d0fa17f
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-12 12:30:43 +0530

    0090-subsets-ii .java

--- LeetCode/Medium/0090-subsets-ii/0090-subsets-ii.java
+++ LeetCode/Medium/0090-subsets-ii/0090-subsets-ii.java
@@ -1,30 +1,27 @@
 class Solution {
-    List<List<Integer>> hs;
+    List<List<Integer>> ans = new ArrayList<>();
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-
-        hs = new ArrayList<>();
         rec(nums, 0, new ArrayList<>());
-        return hs;
+        return ans;
     }
 
     void rec(int[] nums, int idx, List<Integer> ls) {
         if (idx == nums.length) {
-            hs.add(new ArrayList<>(ls));
+            ans.add(new ArrayList<>(ls));
             return;
         }
-
         ls.add(nums[idx]);
 
-        rec(nums, idx + 1, ls);
-
-        ls.remove(ls.size() - 1);
-
         int i = idx + 1;
-        while (i < nums.length && nums[i] == nums[idx])
+        rec(nums, idx+1, ls);
+        
+        ls.remove(ls.size() - 1);
+        while (i < nums.length && nums[i] == nums[idx]) {
             i++;
-
+        }
+        
         rec(nums, i, ls);
     }
 }
