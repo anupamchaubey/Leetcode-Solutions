21e7916a51fc6f8157a7c25b19208116d48435a7
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-13 11:10:11 +0530

    0047-permutations-ii .java

--- LeetCode/Medium/0047-permutations-ii/0047-permutations-ii.java
+++ LeetCode/Medium/0047-permutations-ii/0047-permutations-ii.java
@@ -1,34 +1,28 @@
 class Solution {
-    List<List<Integer>> ans = new ArrayList<>();
-
+    List<List<Integer>> ans=new ArrayList<>();
     public List<List<Integer>> permuteUnique(int[] nums) {
-        boolean[] visited = new boolean[nums.length];
         Arrays.sort(nums);
-        rec(nums, new ArrayList<>(), visited);
+        rec(nums, new ArrayList<>(), new boolean[nums.length]);
         return ans;
     }
-
-    void rec(int[] nums, List<Integer> ls, boolean[] visited) {
-        if (ls.size() == nums.length) {
+    void rec(int[] nums, List<Integer> ls, boolean[] visited){
+        if(ls.size()==nums.length){
             ans.add(new ArrayList<>(ls));
             return;
         }
-        for (int i = 0; i < nums.length; ) {
-            if (!visited[i]) {
-                visited[i] = true;
+        for(int i=0;i<nums.length;){
+            if(!visited[i]){
+                visited[i]=true;
                 ls.add(nums[i]);
                 rec(nums, ls, visited);
-                ls.remove(ls.size() - 1);
-                visited[i] = false;
+                ls.remove(ls.size()-1);
+                visited[i]=false;
                 int idx=i+1;
-                while(idx<nums.length && nums[idx]==nums[i]){
-                    idx++;
-                }
+                while(idx<nums.length && nums[idx]==nums[i])idx++;
                 i=idx;
             }else{
                 i++;
             }
-
         }
     }
 }
