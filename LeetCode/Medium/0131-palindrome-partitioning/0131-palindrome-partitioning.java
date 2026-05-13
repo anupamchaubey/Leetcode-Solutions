723925de4f2698012ccdffa8182fb19e58e4d5af
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-13 11:30:05 +0530

    0131-palindrome-partitioning .java

--- LeetCode/Medium/0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ LeetCode/Medium/0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,39 +1,34 @@
 class Solution {
-    List<List<String>> ans;
     boolean[][] pal;
-
+    List<List<String>> ans=new ArrayList<>();
     public List<List<String>> partition(String s) {
-        ans = new ArrayList<>();
-
-        int n = s.length();
-        pal = new boolean[n][n];
-        for (int i = 0; i < n; i++) {
-            pal[i][i] = true;
+        pal=new boolean[s.length()][s.length()];
+        for(int i=0;i<s.length();i++){
+            pal[i][i]=true;
         }
-        for (int j = 0; j < n; j++) {
-            for (int i = j - 1; i >= 0; i--) {
-                int len = j - i + 1;
-                if (len == 2) {
-                    pal[i][j] = s.charAt(i) == s.charAt(j);
-                } else {
-                    pal[i][j] = (s.charAt(i) == s.charAt(j)) && pal[i + 1][j - 1];
+        for(int j=0;j<s.length();j++){
+            for(int i=j-1;i>=0;i--){
+                int len=j-i+1;
+                if(len==2){
+                    pal[i][j]=s.charAt(i)==s.charAt(j);
+                }else{
+                    pal[i][j]=pal[i+1][j-1] && s.charAt(i)==s.charAt(j);
                 }
             }
         }
-        rec(s, new ArrayList<>(), 0);
+        rec(s, 0, new ArrayList<>());
         return ans;
     }
-
-    void rec(String s, List<String> ls, int st) {
-        if (st == s.length()) {
+    void rec(String s, int idx, List<String> ls){
+        if(idx==s.length()){
             ans.add(new ArrayList<>(ls));
             return;
         }
-        for (int i = st; i < s.length(); i++) {
-            if (pal[st][i]) {
-                ls.add(s.substring(st, i + 1));
-                rec(s, ls, i + 1);
-                ls.remove(ls.size() - 1);
+        for(int i=idx;i<s.length();i++){
+            if(pal[idx][i]){
+                ls.add(s.substring(idx, i+1));
+                rec(s, i+1, ls);
+                ls.remove(ls.size()-1);
             }
         }
     }
