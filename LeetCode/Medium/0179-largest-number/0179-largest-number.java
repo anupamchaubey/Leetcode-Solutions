148add889dfd7b45075cb81b3120135b57b12521
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-19 00:19:55 +0530

    0179-largest-number .java

--- LeetCode/Medium/0179-largest-number/0179-largest-number.java
+++ LeetCode/Medium/0179-largest-number/0179-largest-number.java
@@ -5,11 +5,9 @@ class Solution {
             arr[i] = String.valueOf(nums[i]);
         }
         Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
-        boolean flag=true;
-        for(int i=0;i<arr.length;i++){
-            if(!arr[i].equals("0"))flag=false;
+        if(arr[0].equals("0")){
+            return "0";
         }
-        if(flag)return "0";
         StringBuilder sb = new StringBuilder();
         for (String s : arr) {
             sb.append(s);
