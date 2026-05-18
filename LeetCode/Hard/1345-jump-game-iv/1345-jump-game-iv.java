c58989bdfb5055f0db025bbfbbcb176c5599d1a8
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-18 10:38:40 +0530

    1345-jump-game-iv .java

--- LeetCode/Hard/1345-jump-game-iv/1345-jump-game-iv.java
+++ LeetCode/Hard/1345-jump-game-iv/1345-jump-game-iv.java
@@ -1,48 +1,43 @@
 class Solution {
+    HashMap<Integer, Set<Integer>> hm;
+    
     public int minJumps(int[] arr) {
-        int n = arr.length;
-        if (n <= 1)
-            return 0;
-        HashMap<Integer, Set<Integer>> hm = new HashMap<>(); //arr[i], indexes with value arr[i]
-        for (int i = 0; i < n; i++) {
-            if (hm.containsKey(arr[i])) {
-                hm.get(arr[i]).add(i);
-            } else {
-                hm.put(arr[i], new HashSet<>());
-                hm.get(arr[i]).add(i);
-            }
+        hm=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            hm.putIfAbsent(arr[i], new HashSet<>());
+            hm.get(arr[i]).add(i);
         }
-        boolean[] visited = new boolean[n];
-        visited[0] = true;
+        boolean[] visited=new boolean[arr.length];
+        visited[0]=true;
 
-        Queue<Integer> q = new LinkedList<>();
-        int steps = 0;
+        Queue<Integer> q=new LinkedList<>();
+        int jumps=0;
         q.offer(0);
-        while (!q.isEmpty()) {
-            int size = q.size();
-            for (int p = 0; p < size; p++) {
-                int idx = q.poll();
 
-                if (idx == n - 1)
-                    return steps;
-
-                if (idx + 1 < n && !visited[idx+1]) {
-                    visited[idx + 1] = true;
-                    q.offer(idx + 1);
+        while(!q.isEmpty()){
+            int size=q.size();
+            while(size-->0){
+                int x=q.poll();
+                if(x==arr.length-1)return jumps;
+                if(x-1>=0 && !visited[x-1]){
+                    visited[x-1]=true;
+                    q.offer(x-1);
                 }
-                if (idx - 1 >= 0 && !visited[idx-1]) {
-                    visited[idx - 1] = true;
-                    q.offer(idx - 1);
+                if(x+1<arr.length && !visited[x+1]){
+                    visited[x+1]=true;
+                    q.offer(x+1);
                 }
-                for (int i : hm.get(arr[idx])) {
-                    if (!visited[i]) {
-                        visited[i] = true;
-                        q.offer(i);
+                if(hm.containsKey(arr[x]))
+                for(int idx: hm.get(arr[x])){
+                    if(idx!=x && !visited[idx]){
+                        visited[idx]=true;
+                        q.offer(idx);
                     }
+                    hm.remove(arr[x]);
                 }
-                hm.get(arr[idx]).clear();// imp
+                
             }
-            steps++;
+            jumps++;
         }
         return -1;
     }
