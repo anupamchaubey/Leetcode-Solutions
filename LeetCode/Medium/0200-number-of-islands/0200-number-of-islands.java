789fae124be345fdff362d1b0c1498cd58fa3fbb
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-19 09:58:22 +0530

    0200-number-of-islands .java

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
@@ -1,29 +1,26 @@
 class Solution {
-    int m, n;
-    int[] dr={-1, 1, 0, 0};
-    int[] dc={0, 0, -1, 1};
     public int numIslands(char[][] grid) {
-        m=grid.length;
-        n=grid[0].length;
-        boolean[][] visited=new boolean[m][n];
+        int m=grid.length;
+        int n=grid[0].length;
         int count=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(!visited[i][j] && grid[i][j]=='1'){
-                    dfs(grid, i, j, visited);
+                if(grid[i][j]=='1'){
+                    dfs(grid,i,j);
                     count++;
                 }
             }
         }
         return count;
     }
-    void dfs(char[][] grid, int r, int c, boolean[][] visited){
-        if(r>=m || c>=n || r<0 || c<0 || visited[r][c] || grid[r][c]=='0')return;
-        visited[r][c]=true;
-        for(int i=0;i<4;i++){
-            int nr=r+dr[i];
-            int nc=c+dc[i];
-            dfs(grid, nr, nc, visited);
+    public void dfs(char[][] grid, int i, int j){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]=='0'){
+            return;
         }
+        grid[i][j]='0';
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
     }
 }
