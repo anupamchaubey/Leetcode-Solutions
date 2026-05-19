93a483f7f8a2c8fd04127416a2be5ddecd1ea2af
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-19 10:38:50 +0530

    0200-number-of-islands .java

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
@@ -1,17 +1,59 @@
 class Solution {
+    int[] parent;
+    int[] rank;
+    int find(int x){
+        if(x==parent[x])return x;
+        return parent[x]=find(parent[x]);
+    }
+    boolean union(int x, int y){
+        int parentX=find(x);
+        int parentY=find(y);
+
+        if(parentX == parentY)return false;
+
+        if(rank[parentX]>rank[parentY]){
+            parent[parentY]=parentX;
+        }else if(rank[parentY]>rank[parentX]){
+            parent[parentX]=parentY;
+        }else{
+            parent[parentY]=parentX;
+            rank[parentX]++;
+        }
+        return true;
+    }
     public int numIslands(char[][] grid) {
+
         int m=grid.length;
         int n=grid[0].length;
-        int count=0;
+
+        parent=new int[m*n];
+        rank=new int[m*n];
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int id=i*n+j;
+                parent[id]=id;
+                rank[id]=1;
+            }
+        }
+        int islands=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
-                    dfs(grid,i,j);
-                    count++;
+                    islands++;
+                    int id=i*n+j;
+                    if(i+1<m && grid[i+1][j]=='1'){
+                        int nid=(i+1)*n+j;
+                        if(union(id, nid))islands--;
+                    }
+                    if(j+1<n && grid[i][j+1]=='1'){
+                        int nid=i*n+(j+1);
+                        if(union(id, nid))islands--;
+                    }
                 }
             }
         }
-        return count;
+        return islands;
     }
     public void dfs(char[][] grid, int i, int j){
         if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]=='0'){
