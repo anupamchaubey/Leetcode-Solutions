2f572626411755b98d048787b5f6b5d49e6ff1c3
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-19 22:49:17 +0530

    0746-min-cost-climbing-stairs .java

--- LeetCode/Easy/0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ LeetCode/Easy/0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -3,14 +3,21 @@ class Solution {
         // int one=rec(cost, 0);
         // int sec=rec(cost, 1);
 
-        int[] dp=new int[cost.length];
-        dp[0]=cost[0];
-        dp[1]=cost[1];
+        // int[] dp=new int[cost.length];
+        // dp[0]=cost[0];
+        // dp[1]=cost[1];
+
+        int one=cost[0];
+        int sec=cost[1];
 
         for(int i=2;i<cost.length;i++){
-            dp[i]=Math.min(dp[i-1]+cost[i], dp[i-2]+cost[i]);
+            int x=cost[i]+Math.min(sec, one);
+            one=sec;
+            sec=x;
         }
-        return Math.min(dp[cost.length-1], dp[cost.length-2]);
+        return Math.min(one, sec);
+        
+        // return Math.min(dp[cost.length-1], dp[cost.length-2]);
 
         // return Math.min(one, sec);
     }
