248b2d20ac98258d603ff6f0a048c5e87cecee5a
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-20 11:25:39 +0530

    0091-decode-ways .java

--- LeetCode/Medium/0091-decode-ways/0091-decode-ways.java
+++ LeetCode/Medium/0091-decode-ways/0091-decode-ways.java
@@ -1,23 +1,23 @@
 class Solution {
     public int numDecodings(String s) {
-        Integer[] dp = new Integer[s.length()];
+        int[] dp=new int[s.length()];
+        Arrays.fill(dp, -1);
         return rec(s, 0, dp);
     }
-    int rec(String s, int idx, Integer[] dp){
-        if(idx>s.length())return 0;
+    int rec(String s, int idx, int[] dp){
         if(idx==s.length())return 1;
+        if(idx>s.length())return 0;
         if(s.charAt(idx)=='0')return 0;
-        if(dp[idx]!=null)return dp[idx];
-        int c=0;
+
+        if(dp[idx]!=-1)return dp[idx];
+
+        int ans=rec(s, idx+1, dp);
         if(s.charAt(idx)=='1'){
-            c+=rec(s, idx+1, dp);
-            c+=rec(s, idx+2, dp);
-        }else if(idx+1<s.length() && s.charAt(idx)=='2' && s.charAt(idx+1)<='6'){
-            c+=rec(s, idx+1, dp);
-            c+=rec(s, idx+2, dp);
-        }else{
-            c+=rec(s, idx+1, dp);
+            ans+=rec(s, idx+2, dp);
         }
-        return dp[idx]= c;
+        if(idx+1<s.length() && s.charAt(idx)=='2' && s.charAt(idx+1)<='6'){
+            ans+=rec(s, idx+2, dp);
+        }
+        return dp[idx]=ans;
     }
 }
