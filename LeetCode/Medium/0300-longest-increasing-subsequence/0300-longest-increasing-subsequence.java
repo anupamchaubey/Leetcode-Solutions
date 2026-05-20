a359eb563bceaf947586c577c1841ffb2ae6f330
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-20 12:23:39 +0530

    0300-longest-increasing-subsequence .java

--- LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,24 +1,39 @@
 class Solution {
     int[][] dp;
-    public int lengthOfLIS(int[] nums) {
-        
-        dp=new int[nums.length][nums.length];
-        for(int[] d: dp)Arrays.fill(d, Integer.MAX_VALUE);
 
-        return rec(nums, 0, -1);
+    public int lengthOfLIS(int[] nums) {
+
+        dp = new int[nums.length + 1][nums.length + 1];
+
+        int n = nums.length;
+
+        for(int idx = n-1; idx >= 0; idx--){
+            for (int prev = idx; prev >= -1; prev--) {
+                int ans = 0;
+                if (prev == -1 || nums[idx] > nums[prev]) {
+                    ans = 1 + dp[idx+1][idx+1];
+                }
+                ans = Math.max(ans, dp[idx+1][prev+1]);
+                dp[idx][prev+1]=ans;
+            }
+        }
+        return dp[0][0];
+        // return rec(nums, 0, -1);
     }
-    int rec(int[] nums, int idx, int prev){
-        if(idx==nums.length)return 0;
-        if(prev!=-1 && dp[idx][prev]!=Integer.MAX_VALUE){
+
+    int rec(int[] nums, int idx, int prev) {
+        if (idx == nums.length)
+            return 0;
+        if (prev != -1 && dp[idx][prev] != Integer.MAX_VALUE) {
             return dp[idx][prev];
         }
-        int ans=0;
-        if(prev==-1 || nums[idx]>nums[prev]){
-            ans=1+rec(nums, idx+1, idx);
+        int ans = 0;
+        if (prev == -1 || nums[idx] > nums[prev]) {
+            ans = 1 + rec(nums, idx + 1, idx);
         }
-        ans=Math.max(ans, rec(nums, idx+1, prev));
-        if(prev!=-1){
-            dp[idx][prev]=ans;
+        ans = Math.max(ans, rec(nums, idx + 1, prev));
+        if (prev != -1) {
+            dp[idx][prev] = ans;
         }
         return ans;
     }
