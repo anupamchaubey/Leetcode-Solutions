784d9e20c0332d6c390968f3203be31efaceb016
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-20 21:46:07 +0530

    0673-number-of-longest-increasing-subsequence .java

--- LeetCode/Medium/0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
+++ LeetCode/Medium/0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
@@ -1,29 +1,31 @@
 class Solution {
     public int findNumberOfLIS(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        int[] cnt = new int[n];
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            dp[i] = 1;
-            cnt[i] = 1;
-            for (int prev = 0; prev < i; prev++) {
-                if (nums[prev] < nums[i]) {
-                    if (dp[i] == dp[prev] + 1) {
-                        cnt[i] += cnt[prev];
-                    } else if (dp[prev] + 1 > dp[i]) {
-                        dp[i] = dp[prev] + 1;
-                        cnt[i] = cnt[prev];
+        int n=nums.length;
+
+        int[] dp=new int[n];
+        int[] cnt=new int[n];
+
+        int max=1;
+
+        for(int i=0;i<n;i++){
+            dp[i]=1;
+            cnt[i]=1;
+            for(int prev=0;prev<i;prev++){
+                if(nums[prev]<nums[i]){
+                    if(dp[prev]+1>dp[i]){
+                        cnt[i]=cnt[prev];
+                        dp[i]=dp[prev]+1;
+                    }else if(dp[prev]+1==dp[i]){
+                        cnt[i]++;
                     }
                 }
             }
-            max = Math.max(max, dp[i]);
+            max=Math.max(max, dp[i]);
         }
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (dp[i] == max)
-                count += cnt[i];
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(dp[i]==max)ans+=cnt[i];
         }
-        return count;
+        return ans;
     }
 }
