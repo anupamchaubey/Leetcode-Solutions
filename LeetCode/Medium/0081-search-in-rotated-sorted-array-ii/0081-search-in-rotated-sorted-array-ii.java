c9544cbdc19052886cae070c31d9755618bddb66
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-22 09:36:55 +0530

    0081-search-in-rotated-sorted-array-ii .java

---
+++ LeetCode/Medium/0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        int l = 0, r = n - 1;
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+
+            if (target == nums[mid])
+                return true;
+
+            if (nums[l] < nums[mid]) { // left sorted
+
+                if (target < nums[mid] && target >= nums[l]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+
+            } else if (nums[mid] < nums[r]) { // right sorted
+
+                if (target > nums[mid] && target <= nums[r]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+
+            } else {
+                if(target!=nums[r])r--;
+                else l++;
+            }
+        }
+
+        return false;
+    }
+}
