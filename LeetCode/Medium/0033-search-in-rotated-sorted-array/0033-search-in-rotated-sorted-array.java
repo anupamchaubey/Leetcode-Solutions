ec988177254a4c11882b7bdae0744b8a3db0b340
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-22 08:37:56 +0530

    0033-search-in-rotated-sorted-array .java

--- LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,19 +1,24 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int l=0, r=nums.length-1;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(target==nums[mid]){
+        int n = nums.length;
+        int l = 0, r = n - 1;
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (target == nums[mid])
                 return mid;
-            }
-            if(nums[l]<=nums[mid]){
-                if(target<nums[mid] && target>=nums[l]){
-                    r=mid-1;
-                }else l=mid+1;
-            }else{
-                if(target>nums[mid] && target<=nums[r]){
-                    l=mid+1;
-                }else r=mid-1;
+            if (nums[l] <= nums[mid]) {//left sorted
+                if (target < nums[mid] && target >= nums[l]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else if (nums[mid] <= nums[r]) {//right sorted
+                if (target > nums[mid] && target <= nums[r]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
             }
         }
         return -1;
