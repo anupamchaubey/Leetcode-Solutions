2cb88fee2784f99c0fca86352a6f7576d73e914a
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-23 12:17:11 +0530

    1312-minimum-insertion-steps-to-make-a-string-palindrome .java

--- LeetCode/Hard/1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ LeetCode/Hard/1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,19 +1,18 @@
 class Solution {
-    int[][] memo;
-    public int minInsertions(String s) {
-        memo=new int[s.length()][s.length()];
-        for(int[] m: memo)Arrays.fill(m, -1);
-        StringBuilder sb=new StringBuilder(s);
-        sb.reverse();
-        return s.length()-rec(s, sb.toString(), 0, 0);
-    }
-    int rec(String s1, String s2, int i, int j){
-        if(i==s1.length() || j==s2.length())return 0;
-        if(memo[i][j]!=-1)return memo[i][j];
-        if(s1.charAt(i)==s2.charAt(j)){
-            return memo[i][j]= 1+rec(s1, s2, i+1, j+1);
-        }else{
-            return memo[i][j]= Math.max(rec(s1, s2, i+1, j), rec(s1, s2, i, j+1));
+    public int minInsertions(String s1) {
+        int[][] dp = new int[s1.length()+1][s1.length()+1];
+        
+        String s2=new StringBuilder(s1).reverse().toString();
+
+        for(int i=1;i<=s1.length();i++){
+            for(int j=1;j<=s2.length();j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
         }
+        return s1.length()-dp[s1.length()][s1.length()];
     }
 }
