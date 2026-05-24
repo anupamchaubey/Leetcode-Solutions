02cca42e06075b389950f580c76b1e335504428e
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-24 11:52:15 +0530

    1340-jump-game-v .java

---
+++ LeetCode/Hard/1340-jump-game-v/1340-jump-game-v.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxJumps(int[] arr, int d) {
+        int max = 1;
+
+        for (int i = 0; i < arr.length; i++) {
+            int[] dp = new int[arr.length];
+            max = Math.max(max, rec(arr, d, i, dp));
+        }
+        return max;
+    }
+
+    int rec(int[] arr, int d, int idx, int[] dp) {
+        if (dp[idx] != 0)
+            return dp[idx];
+
+        int ans = 1;
+        for (int i = idx + 1; i <= Math.min(arr.length - 1, idx + d); i++) {
+            if (arr[i] >= arr[idx])
+                break;
+            ans = Math.max(ans, 1 + rec(arr, d, i, dp));
+        }
+        for (int i = idx - 1; i >= Math.max(0, idx - d); i--) {
+            if (arr[i] >= arr[idx])
+                break;
+            ans = Math.max(ans, 1 + rec(arr, d, i, dp));
+        }
+        return dp[idx] = ans;
+    }
+}
