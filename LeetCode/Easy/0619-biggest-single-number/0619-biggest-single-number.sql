a0daff6593b0f286c443704eb7dee215065d66c5
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-24 21:07:59 +0530

    0619-biggest-single-number .sql

--- LeetCode/Easy/0619-biggest-single-number/0619-biggest-single-number.sql
+++ LeetCode/Easy/0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,9 +1,10 @@
 # Write your MySQL query statement below
 
-select max(s.nu) as num
-From (
-    Select num as nu
+select(
+    select num
 From MyNumbers
 Group By num
 Having count(num)=1
-) as s
+Order By num Desc
+limit 1
+) as num
