c9f33cca46c9171a495f3b02192131934da5aaa2
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-05-29 00:40:56 +0530

    2367-number-of-arithmetic-triplets .java

--- LeetCode/Easy/2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.java
+++ LeetCode/Easy/2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.java
@@ -1,14 +1,20 @@
 class Solution {
     public int arithmeticTriplets(int[] nums, int diff) {
-        int count=0;
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[j]-nums[i]!=diff)continue;
-                for(int k=j+1;k<nums.length;k++){
-                    if(nums[k]-nums[j]!=diff)continue;
-                    count++;
-                }
+        int count = 0;
+        int k = nums.length - 1;
+        while (k >= 2) {
+            int j = k - 1;
+            while (j>0 && nums[k] - nums[j] < diff)
+                j--;
+            if (nums[k] - nums[j] != diff) {
+                k--;
+                continue;
             }
+            for (int i = 0; i < j; i++) {
+                if (nums[j] - nums[i] == diff)
+                    count++;
+            }
+            k--;
         }
         return count;
     }
