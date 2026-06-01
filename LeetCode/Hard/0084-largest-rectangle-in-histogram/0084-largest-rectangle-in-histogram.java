5cbffa86dad19003fb216ca38aba7a239a64490c
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-06-01 11:31:50 +0530

    0084-largest-rectangle-in-histogram .java

--- LeetCode/Hard/0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ LeetCode/Hard/0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,39 +1,34 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int max = 0;
-        int n = heights.length;
-
-        Stack<Integer> st = new Stack<>();
-        Stack<Integer> st1 = new Stack<>();
-        int[] l = new int[n];
-        int[] r = new int[n];
-
-        for (int i = 0; i < n; i++) {
-
+        int[] r = new int[heights.length];
+        Deque<Integer> st = new ArrayDeque<>();
+        for (int i = heights.length - 1; i >= 0; i--) {
+            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
+                st.pop();
+            }
+            if (st.isEmpty()) {
+                r[i] = heights.length;
+            } else
+                r[i] = st.peek();
+            st.push(i);
+        }
+        st = new ArrayDeque<>();
+        int[] l = new int[heights.length];
+        for (int i = 0; i < heights.length; i++) {
             while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
                 st.pop();
             }
             if (st.isEmpty()) {
                 l[i] = -1;
-            } else {
+            } else
                 l[i] = st.peek();
-            }
             st.push(i);
         }
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st1.isEmpty() && heights[st1.peek()] >= heights[i]) {
-                st1.pop();
-            }
-            if (st1.isEmpty()) {
-                r[i] = n;
-            } else {
-                r[i] = st1.peek();
-            }
-            st1.push(i);
-        }
-
-        for (int i = 0; i < n; i++) {
-            max = Math.max(max, (r[i] - l[i] - 1) * heights[i]);
+        int max = 0;
+        for (int i = 0; i < heights.length; i++) {
+            int width = (i - l[i] - 1);
+            width += (r[i] - i);
+            max = Math.max(max, heights[i] * width);
         }
         return max;
     }
