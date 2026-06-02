2e40518786de8b6ae236d6d74f69e47af3a7a728
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-06-02 09:44:34 +0530

    0735-asteroid-collision .java

--- LeetCode/Medium/0735-asteroid-collision/0735-asteroid-collision.java
+++ LeetCode/Medium/0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,20 +1,27 @@
 class Solution {
-    public int[] asteroidCollision(int[] asteroids) {
-        Deque<Integer> st=new ArrayDeque<>();
-        for(int i=0;i<asteroids.length;i++){
-            if(asteroids[i]>0){
+    public int[] asteroidCollision(int[] rocks) {
+        Deque<Integer> st = new ArrayDeque<>();
+        for (int i = 0; i < rocks.length; i++) {
+            if (rocks[i] > 0) {
                 st.push(i);
-            }else{
-                while(!st.isEmpty() && asteroids[st.peek()]>0 && asteroids[st.peek()]<Math.abs(asteroids[i]))st.pop();
-                if(!st.isEmpty() && asteroids[st.peek()]>0 && asteroids[st.peek()]==Math.abs(asteroids[i]))st.pop();
-                else if(!st.isEmpty() && asteroids[st.peek()]>0 && asteroids[st.peek()]>Math.abs(asteroids[i]))continue;
-                else st.push(i);
+                continue;
+            } else {
+                while (!st.isEmpty() && rocks[st.peek()] > 0 && Math.abs(rocks[i]) > rocks[st.peek()]) {
+                    st.pop();
+                }
+                if (!st.isEmpty() && rocks[st.peek()] > 0 && Math.abs(rocks[i]) == rocks[st.peek()]) {
+                    st.pop();
+                    continue;
+                }
+                if (st.isEmpty() || rocks[st.peek()] < 0) {
+                    st.push(i);
+                }
             }
         }
-        int[] ans=new int[st.size()];
-        for(int i=ans.length-1;i>=0;i--){
-            ans[i]=asteroids[st.pop()];
+        int[] arr = new int[st.size()];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            arr[i] = rocks[st.pop()];
         }
-        return ans;
+        return arr;
     }
 }
