08b76688585002f23f2e53b6b290bbb86d2e7498
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-06-03 10:46:45 +0530

    0503-next-greater-element-ii .java

--- LeetCode/Medium/0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ LeetCode/Medium/0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,31 +1,14 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-
+        int n=nums.length;
+        int[] ans=new int[n];
         Deque<Integer> st = new ArrayDeque<>();
-        Deque<Integer> lt = new ArrayDeque<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (lt.isEmpty() || nums[i] > nums[lt.peekFirst()]) {
-                lt.addLast(i);
+        for(int i=2*n-1;i>=0;i--){
+            while(!st.isEmpty() && nums[st.peek()]<=nums[i%n])st.pop();
+            if(i<n){
+                ans[i]=(st.isEmpty())? -1: nums[st.peek()];
             }
-        }
-        int[] ans = new int[nums.length];
-        for (int i = nums.length - 1; i >= 0; i--) {
-            while (!st.isEmpty() && nums[i] >= nums[st.peek()])
-                st.pop();
-            if (st.isEmpty()) {
-                while (!lt.isEmpty() && nums[i] >= nums[lt.peekFirst()]) {
-                    if (lt.peekFirst() >= i)
-                        break;
-                    lt.pollFirst();
-                }
-                if (!lt.isEmpty() && lt.peekFirst() < i) {
-                    ans[i] = nums[lt.peekFirst()];
-                } else {
-                    ans[i] = -1;
-                }
-            } else
-                ans[i] = nums[st.peek()];
-            st.push(i);
+            st.push(i%n);
         }
         return ans;
     }
