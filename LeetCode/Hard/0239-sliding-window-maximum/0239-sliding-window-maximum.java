011bd2d2041cf2941b4532599374e90bfd1d31cc
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-06-04 10:12:08 +0530

    0239-sliding-window-maximum .java

--- LeetCode/Hard/0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ LeetCode/Hard/0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,24 +1,27 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        
-        int n=nums.length;
-        int[] arr= new int[n-k+1];
-
-        Deque<Integer> dq=new ArrayDeque<>();
-
-        for(int i=0;i<n;i++){
-
-            while(!dq.isEmpty() && dq.peekFirst()<=(i-k)){
-                dq.pollFirst();
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
+        int idx = 0;
+        Deque<Integer> q = new ArrayDeque<>();
+        int l = 0;
+        for (int i = 0; i < n; i++) {
+            while (!q.isEmpty() && nums[q.peekLast()] <= nums[i]) {
+                q.pollLast();
             }
-            while(!dq.isEmpty() && nums[dq.peekLast()]<=nums[i]){
-                dq.pollLast();
+            q.addLast(i);
+
+            while ((i - l + 1) > k) {
+                if (q.peekFirst() <= l) {
+                    q.pollFirst();
+                }
+                l++;
+
             }
-            dq.addLast(i);
-            if(i>=k-1){
-                arr[i-k+1]=nums[dq.peekFirst()];
-            }
+            if (i < k - 1)
+                continue;
+            ans[idx++] = (q.isEmpty()) ? -1 : nums[q.peekFirst()];
         }
-        return arr;
+        return ans;
     }
 }
