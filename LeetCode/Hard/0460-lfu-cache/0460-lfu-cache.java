1201cc71036f794dc0faad6eb8167e3be8b9d720
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-06-04 11:38:05 +0530

    0460-lfu-cache .java

--- LeetCode/Hard/0460-lfu-cache/0460-lfu-cache.java
+++ LeetCode/Hard/0460-lfu-cache/0460-lfu-cache.java
@@ -2,65 +2,77 @@ class LFUCache {
     Map<Integer, Integer> freq;
     PriorityQueue<Integer> pq;
     Map<Integer, Integer> hm;
-    int cnt = 0;
     Map<Integer, Integer> fr;
+
+    int cnt = 0;
     int k;
 
     public LFUCache(int capacity) {
         k = capacity;
+
         freq = new HashMap<>();
-        pq = new PriorityQueue<>((a, b) -> {
-            if (freq.get(a).equals(freq.get(b)))
-                return Integer.compare(fr.get(a), fr.get(b));
-            return Integer.compare(freq.get(a), freq.get(b));
-        });
         hm = new HashMap<>();
         fr = new HashMap<>();
+
+        pq = new PriorityQueue<>((a, b) -> {
+            if (!freq.get(a).equals(freq.get(b))) {
+                return Integer.compare(freq.get(a), freq.get(b));
+            }
+            return Integer.compare(fr.get(a), fr.get(b));
+        });
     }
 
     public int get(int key) {
-        if (hm.containsKey(key)) {
-            pq.remove(key);
-            freq.put(key, freq.get(key) + 1);
-            cnt++;
-            fr.put(key, cnt);
-            pq.offer(key);
-            return hm.get(key);
-        }
-        return -1;
+        if (!hm.containsKey(key))
+            return -1;
+
+        pq.remove(key);
+
+        freq.put(key, freq.get(key) + 1);
+
+        cnt++;
+        fr.put(key, cnt);
+
+        pq.offer(key);
+
+        return hm.get(key);
     }
 
     public void put(int key, int value) {
+
         if (k == 0)
             return;
 
         if (hm.containsKey(key)) {
+
             pq.remove(key);
-            freq.put(key, freq.get(key) + 1);
+
             hm.put(key, value);
+            freq.put(key, freq.get(key) + 1);
+
             cnt++;
             fr.put(key, cnt);
+
             pq.offer(key);
+
             return;
-        } else if (hm.size() == k) {
+        }
+
+        if (hm.size() == k) {
+
             int removeKey = pq.poll();
+
             hm.remove(removeKey);
             freq.remove(removeKey);
             fr.remove(removeKey);
-
         }
+
         hm.put(key, value);
         freq.put(key, 1);
 
         cnt++;
         fr.put(key, cnt);
+
         pq.offer(key);
     }
 }
-
-/**
- * Your LFUCache object will be instantiated and called as such:
- * LFUCache obj = new LFUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
