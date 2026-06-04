72df237fc9f83764fa245f007af50e4d4638df74
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-06-04 13:41:27 +0530

    0142-linked-list-cycle-ii .java

--- LeetCode/Medium/0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ LeetCode/Medium/0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,24 +11,23 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        if (head == null || head.next == null)
-            return null;
-        ListNode fast = head;
         ListNode slow = head;
-
+        ListNode fast = head;
         while (fast != null && fast.next != null) {
-            fast = fast.next.next;
+
+            
             slow = slow.next;
+            fast = fast.next.next;
+
             if (slow == fast) {
-                while (head != slow) {
-                    head = head.next;
+                slow=head;
+                while (slow != fast) {
                     slow = slow.next;
+                    fast = fast.next;
                 }
-                return head;
+                return slow;
             }
-
         }
-
         return null;
     }
 }
