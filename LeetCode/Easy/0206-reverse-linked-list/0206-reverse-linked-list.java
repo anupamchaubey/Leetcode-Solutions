4ab8d66b17aab60e6f12a64b08ba33c6a244b179
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-06-04 13:23:02 +0530

    0206-reverse-linked-list .java

--- LeetCode/Easy/0206-reverse-linked-list/0206-reverse-linked-list.java
+++ LeetCode/Easy/0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -11,16 +11,24 @@
 class Solution {
     public ListNode reverseList(ListNode head) {
         
-        ListNode prev=null;
-        ListNode curr=head;
+        // ListNode prev=null;
+        // ListNode curr=head;
 
-        while(curr!=null){
-            ListNode next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
-        }
+        // while(curr!=null){
+        //     ListNode next=curr.next;
+        //     curr.next=prev;
+        //     prev=curr;
+        //     curr=next;
+        // }
 
-        return prev;
+        //return prev;
+        return reverse(head, null);
+    }
+    ListNode reverse(ListNode curr, ListNode prev){
+        if(curr==null)return prev;
+        ListNode next=curr.next;
+        
+        curr.next=prev;
+        return reverse(next, curr);
     }
 }
