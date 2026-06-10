c13eea9bc1b11bbea0751fa0e90e59007c0fbac7
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-06-10 11:34:55 +0530

    3952-maximum-total-value-of-covered-indices .java

--- LeetCode/Medium/3952-maximum-total-value-of-covered-indices/3952-maximum-total-value-of-covered-indices.java
+++ LeetCode/Medium/3952-maximum-total-value-of-covered-indices/3952-maximum-total-value-of-covered-indices.java
@@ -1,25 +1,42 @@
 class Solution {
-    Long[][] dp;
     public long maxTotal(int[] nums, String s) {
-        dp=new Long[s.length()][2];
-        long ans = rec(nums, s, 1, (s.charAt(0) == '1') ? 1 : 0);
-        ans += (s.charAt(0) == '1') ? nums[0] : 0;
-        return ans;
-    }
-
-    long rec(int[] nums, String s, int idx, int lastUsed) {
-        if (idx == s.length())
+        if (s.isBlank())
             return 0;
-        if(dp[idx][lastUsed]!=null)return dp[idx][lastUsed];
         long ans = 0;
-        if (s.charAt(idx) == '1') {
-            if (lastUsed==0) {
-                ans = nums[idx - 1] + rec(nums, s, idx + 1, 0);
-            }
-            ans = Math.max(ans, nums[idx] + rec(nums, s, idx + 1, 1));
-        }else{
-            return dp[idx][lastUsed]= rec(nums, s, idx + 1, 0);
+        boolean[] used = new boolean[s.length()];
+
+        if (s.charAt(0) == '1') {
+            ans += nums[0];
+            used[0] = true;
         }
-        return dp[idx][lastUsed]= ans;
+        int i = 1;
+        while (i < s.length()) {
+            if (s.charAt(i) == '1') {
+                if (used[i - 1]) {
+                    ans += nums[i];
+                    used[i] = true;
+                    i++;
+                    continue;
+                }
+                int min = -1;
+                while (i < s.length() && s.charAt(i) == '1') {
+                    if (min == -1 || nums[i - 1] < nums[min]) {
+                        min = i - 1;
+                    }
+                    used[i - 1] = true;
+                    ans += nums[i - 1];
+                    i++;
+                }
+                ans += nums[i - 1];
+                if (min == -1 || nums[i - 1] < nums[min]) {
+                    min = i - 1;
+                }
+                used[i - 1] = true;
+                ans -= nums[min];
+                used[min] = false;
+            }
+            i++;
+        }
+        return ans;
     }
 }
