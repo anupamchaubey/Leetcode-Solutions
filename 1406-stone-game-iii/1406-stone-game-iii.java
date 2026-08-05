d0ba8ddcf41b1b3d72c7d828215568f0fcc33e33
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-08-05 14:48:58 +0530

    Time: 103 ms (5.15%), Space: 104.6 MB (5.22%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -3,10 +3,8 @@ class Solution {
 
     public String stoneGameIII(int[] stones) {
         dp = new Integer[stones.length][2];
-        int totalSum = 0;
-        for (int x : stones)
-            totalSum += x;
-        int x = rec(stones, totalSum, 0, 1);
+        
+        int x = rec(stones, 0, 1);
         if (x > 0)
             return "Alice";
         else if (x < 0)
@@ -14,7 +12,7 @@ class Solution {
         return "Tie";
     }
 
-    int rec(int[] stones, int total, int idx, int alice) {
+    int rec(int[] stones, int idx, int alice) {
         if (idx == stones.length) {
             return 0;
         }
@@ -22,18 +20,18 @@ class Solution {
             return dp[idx][alice];
         if (alice == 1) {
 
-            int max = stones[idx] + rec(stones, total, idx + 1, 0);
+            int max = stones[idx] + rec(stones, idx + 1, 0);
             if (idx + 1 < stones.length)
-                max = Math.max(max, stones[idx] + stones[idx + 1] + rec(stones, total, idx + 2, 0));
+                max = Math.max(max, stones[idx] + stones[idx + 1] + rec(stones, idx + 2, 0));
             if (idx + 2 < stones.length)
-                max = Math.max(max, stones[idx] + stones[idx + 1] + stones[idx + 2] + rec(stones, total, idx + 3, 0));
+                max = Math.max(max, stones[idx] + stones[idx + 1] + stones[idx + 2] + rec(stones, idx + 3, 0));
             return dp[idx][alice] = max;
         } else {
-            int min = -stones[idx] + rec(stones, total, idx + 1, 1);
+            int min = -stones[idx] + rec(stones, idx + 1, 1);
             if (idx + 1 < stones.length)
-                min = Math.min(min, -stones[idx] - stones[idx + 1] + rec(stones, total, idx + 2, 1));
+                min = Math.min(min, -stones[idx] - stones[idx + 1] + rec(stones, idx + 2, 1));
             if (idx + 2 < stones.length)
-                min = Math.min(min, -stones[idx] - stones[idx + 1] - stones[idx + 2] + rec(stones, total, idx + 3, 1));
+                min = Math.min(min, -stones[idx] - stones[idx + 1] - stones[idx + 2] + rec(stones, idx + 3, 1));
 
             return dp[idx][alice] = min;
         }
