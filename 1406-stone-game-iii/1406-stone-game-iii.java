6ca68581b4cdbaf606e61d551788aacb293e3744
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-08-05 14:49:16 +0530

    Time: 31 ms (24.23%), Space: 95.7 MB (5.22%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,10 +1,9 @@
 class Solution {
-    Integer[][] dp;
+    Integer[] dp;
 
     public String stoneGameIII(int[] stones) {
-        dp = new Integer[stones.length][2];
-        
-        int x = rec(stones, 0, 1);
+        dp = new Integer[stones.length];
+        int x = rec(stones, 0);
         if (x > 0)
             return "Alice";
         else if (x < 0)
@@ -12,28 +11,18 @@ class Solution {
         return "Tie";
     }
 
-    int rec(int[] stones, int idx, int alice) {
+    int rec(int[] stones, int idx) {
         if (idx == stones.length) {
             return 0;
         }
-        if (dp[idx][alice] != null)
-            return dp[idx][alice];
-        if (alice == 1) {
-
-            int max = stones[idx] + rec(stones, idx + 1, 0);
-            if (idx + 1 < stones.length)
-                max = Math.max(max, stones[idx] + stones[idx + 1] + rec(stones, idx + 2, 0));
-            if (idx + 2 < stones.length)
-                max = Math.max(max, stones[idx] + stones[idx + 1] + stones[idx + 2] + rec(stones, idx + 3, 0));
-            return dp[idx][alice] = max;
-        } else {
-            int min = -stones[idx] + rec(stones, idx + 1, 1);
-            if (idx + 1 < stones.length)
-                min = Math.min(min, -stones[idx] - stones[idx + 1] + rec(stones, idx + 2, 1));
-            if (idx + 2 < stones.length)
-                min = Math.min(min, -stones[idx] - stones[idx + 1] - stones[idx + 2] + rec(stones, idx + 3, 1));
-
-            return dp[idx][alice] = min;
+        if (dp[idx] != null)
+            return dp[idx];
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = idx; i < stones.length && i < idx + 3; i++) {
+            sum += stones[i];
+            max = Math.max(max, sum - rec(stones, i + 1));
         }
+        return dp[idx] = max;
     }
 }
