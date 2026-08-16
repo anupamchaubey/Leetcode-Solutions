acea19ea717bc0b39f57230f25e9ba81d897c94a
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-08-17 02:47:23 +0530

    Time: 3 ms (21.36%), Space: 47.2 MB (73.89%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,12 +1,22 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            int val = 1;
-            for (int j = i; j < nums.length; j++) {
-                val *= nums[j];
-                max = Math.max(max, val);
-            }
+        int max = nums[0];
+        int maxTillHere = nums[0];
+        int minTillHere = nums[0];
+
+        //in this for loop, 
+        //every time we introduce a new element, we have three choices, 
+        //start a new subarray for product
+        //multiply the current element with minimum till now 
+        //multiply the current element with maximum till now
+        // we will take the maximum of these three each time we introduce a new element and store it in max which will be our result
+
+        for (int i = 1; i < nums.length; i++) {
+            int currMax = Math.max(maxTillHere * nums[i], minTillHere * nums[i]);
+            int currMin = Math.min(minTillHere * nums[i], maxTillHere * nums[i]);
+            max = Math.max(max, Math.max(nums[i], currMax));
+            maxTillHere = Math.max(currMax, nums[i]);
+            minTillHere = Math.min(currMin, nums[i]);
         }
         return max;
     }
