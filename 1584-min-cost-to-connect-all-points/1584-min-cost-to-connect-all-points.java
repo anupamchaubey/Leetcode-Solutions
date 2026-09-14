7cd14291382fdab28013cf05cbbfefcb6dd3443b
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-09-15 01:11:30 +0530

    Time: 293 ms (53.05%), Space: 82.3 MB (41.96%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -0,0 +1,71 @@
+class Solution {
+    int[] parent;
+    int[] rank;
+
+    int find(int x) {
+        if (parent[x] == x)
+            return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    boolean union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+        if (pa == pb)
+            return false;
+        if (rank[pa] > rank[pb]) {
+            parent[pb] = parent[pa];
+        } else if (rank[pb] > rank[pa]) {
+            parent[pa] = parent[pb];
+        } else {
+            parent[pa] = parent[pb];
+            rank[pb]++;
+        }
+        return true;
+    }
+
+    class Edge {
+        int u;
+        int v;
+        int w;
+
+        Edge(int x, int y, int dist) {
+            this.u = x;
+            this.v = y;
+            this.w = dist;
+        }
+    }
+
+    public int minCostConnectPoints(int[][] points) {
+        int n = points.length;
+        List<Edge> edges = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int[] p1 = points[i];
+                int[] p2 = points[j];
+                int dist = Math.abs(p1[0] - p2[0]) + Math.abs(p1[1] - p2[1]);
+                edges.add(new Edge(i, j, dist));
+            }
+        }
+        edges.sort((a, b) -> a.w - b.w);
+        parent = new int[n];
+        rank = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        int edgesUsed = 0;
+        int totalCost = 0;
+        for (Edge e : edges) {
+            if (union(e.u, e.v)) {
+                totalCost += e.w;
+                edgesUsed++;
+            }
+
+            if (edgesUsed == n - 1)
+                break;
+        }
+        return totalCost;
+    }
+}
