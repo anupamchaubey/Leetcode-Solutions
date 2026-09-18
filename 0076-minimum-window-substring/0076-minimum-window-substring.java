8be331a72893552793bbd7dabc82efe6c212cf14
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-09-19 00:06:43 +0530

    Time: 108 ms (13.23%), Space: 47.8 MB (18.74%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,34 +1,27 @@
 class Solution {
     public String minWindow(String s, String t) {
-        HashMap<Character, Integer> hm = new HashMap<>();
+        int[] arrT = new int[126];
         for (char ch : t.toCharArray()) {
-            hm.put(ch, hm.getOrDefault(ch, 0) - 1);
+            arrT[ch]--;
         }
-        int min = Integer.MAX_VALUE;
-        int st = 0, en = 0;
+        String ans = s + t;
+        int cnt = 0;
 
-        int val = -hm.size();
         int l = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (!hm.containsKey(s.charAt(i)))
-                continue;
-            hm.put(s.charAt(i), hm.get(s.charAt(i)) + 1);
-            if (hm.get(s.charAt(i)) == 0)
-                val++;
-            while (val == 0) {
-                if (hm.containsKey(s.charAt(l))) {
-                    hm.put(s.charAt(l), hm.get(s.charAt(l)) - 1);
-                    if (hm.get(s.charAt(l)) < 0)
-                        val--;
-                }
-                if (i - l + 1 < min) {
-                    min = i - l + 1;
-                    st = l;
-                    en = i + 1;
-                }
+            arrT[s.charAt(i)]++;
+            if (arrT[s.charAt(i)] <= 0)
+                cnt++;
+
+            while (cnt == t.length()) {
+                if (ans.length() > i - l + 1)
+                    ans = s.substring(l, i + 1);
+                arrT[s.charAt(l)]--;
+                if (arrT[s.charAt(l)] < 0)
+                    cnt--;
                 l++;
             }
         }
-        return s.substring(st, en);
+        return (ans.length() > s.length()) ? "" : ans;
     }
 }
