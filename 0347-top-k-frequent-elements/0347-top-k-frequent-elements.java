7de7f57b88ab0af30b54c9cee7a3e27c9531ecac
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-09-18 17:15:38 +0530

    Time: 11 ms (96.14%), Space: 47.8 MB (33.21%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,17 +1,30 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        // we will use the bucket sort
         HashMap<Integer, Integer> hm = new HashMap<>();
         for (int x : nums) {
             hm.put(x, hm.getOrDefault(x, 0) + 1);
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> Integer.compare(hm.get(b), hm.get(a)));
 
-        for (int x : hm.keySet()) {
-            pq.offer(x);
+        List<Integer>[] bucket = new ArrayList[nums.length + 1];
+
+        for (int key : hm.keySet()) {
+            if (bucket[hm.get(key)] == null)
+                bucket[hm.get(key)] = new ArrayList<>();
+            bucket[hm.get(key)].add(key);
         }
+
+        int l = nums.length;
+
+        int i = 0;
         int[] ans = new int[k];
-        for (int i = 0; i < k; i++) {
-            ans[i] = pq.poll();
+        while (i < k) {
+            while (bucket[l] == null || bucket[l].size() == 0) {
+                l--;
+            }
+            ans[i] = bucket[l].get(0);
+            bucket[l].remove(0);
+            i++;
         }
         return ans;
     }
