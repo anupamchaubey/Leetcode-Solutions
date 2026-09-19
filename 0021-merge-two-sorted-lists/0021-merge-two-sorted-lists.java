d7de201befbc239822b5e591900a89175f1117eb
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-09-19 14:45:59 +0530

    Time: 0 ms (100%), Space: 44 MB (87.58%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,23 +10,38 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode node = new ListNode(0);
-        ListNode temp = node;
-        while (list1 != null && list2 != null) {
-            if (list1.val < list2.val) {
-                temp.next = list1;
-                temp = temp.next;
-                list1 = list1.next;
-            } else {
-                temp.next = list2;
-                temp = temp.next;
-                list2 = list2.next;
-            }
+        // ListNode node = new ListNode(0);
+        // ListNode temp = node;
+        // while (list1 != null && list2 != null) {
+        //     if (list1.val < list2.val) {
+        //         temp.next = list1;
+        //         temp = temp.next;
+        //         list1 = list1.next;
+        //     } else {
+        //         temp.next = list2;
+        //         temp = temp.next;
+        //         list2 = list2.next;
+        //     }
+        // }
+        // if (list1 != null)
+        //     temp.next = list1;
+        // if (list2 != null)
+        //     temp.next = list2;
+        // return node.next;
+        return rec(list1, list2);
+    }
+
+    ListNode rec(ListNode list1, ListNode list2) {
+        if (list1 == null)
+            return list2;
+        if (list2 == null)
+            return list1;
+        if (list1.val < list2.val) {
+            list1.next = rec(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = rec(list1, list2.next);
+            return list2;
         }
-        if (list1 != null)
-            temp.next = list1;
-        if (list2 != null)
-            temp.next = list2;
-        return node.next;
     }
 }
