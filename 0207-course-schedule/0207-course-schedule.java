ac2601bd376eb07fa8e7c65dbe92a0c04628c77a
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-09-21 20:44:45 +0530

    Time: 4 ms (91.55%), Space: 47.5 MB (7.76%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,66 +1,28 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        // ** Kahn's Algorithm ** 
-        // int[] indegree = new int[numCourses];
-        // List<List<Integer>> adj = new ArrayList<>();
-        // for (int i = 0; i < numCourses; i++)
-        //     adj.add(new ArrayList<>());
-
-        // for (int[] arr : prerequisites) {
-        //     indegree[arr[0]]++;
-        //     adj.get(arr[1]).add(arr[0]);
-        // }
-        // Deque<Integer> q = new ArrayDeque<>();
-        // for (int i = 0; i < indegree.length; i++) {
-        //     if (indegree[i] == 0)
-        //         q.offer(i);
-        // }
-        // while (!q.isEmpty()) {
-        //     int x = q.poll();
-        //     for (int v : adj.get(x)) {
-        //         indegree[v]--;
-        //         if (indegree[v] == 0) {
-        //             q.offer(v);
-        //         }
-        //     }
-        // }
-        // for (int x : indegree)
-        //     if (x > 0)
-        //         return false;
-        // return true;
-
-        // ** Cycle detection Algorithm in Directed Graph **
-
         List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++)
-            adj.add(new ArrayList<>());
-        for (int[] arr : prerequisites) {
-            adj.get(arr[1]).add(arr[0]);
-        }
-        boolean[] visited = new boolean[numCourses];
-        boolean[] samePath = new boolean[numCourses];
+        for (int i = 0; i < numCourses; i++) adj.add(new ArrayList<>());
 
-        for (int i = 0; i < numCourses; i++) {
-            if (!visited[i]) {
-                if (cycle(adj, i, visited, samePath))
-                    return false;
-            }
-        }
+        for (int[] pre : prerequisites)
+            adj.get(pre[1]).add(pre[0]);
+
+        boolean[] vis = new boolean[numCourses];
+        boolean[] path = new boolean[numCourses];
+
+        for (int i = 0; i < numCourses; i++)
+            if (!vis[i] && dfs(i, adj, vis, path)) return false;
+
         return true;
     }
 
-    boolean cycle(List<List<Integer>> adj, int v, boolean[] visited, boolean[] samePath) {
-        visited[v] = true;
-        samePath[v] = true;
-        for (int x : adj.get(v)) {
-            if (!visited[x]) {
-                if (cycle(adj, x, visited, samePath))
-                    return true;
-            } else if (samePath[x]) {
-                return true;
-            }
-        }
-        samePath[v] = false;
+    private boolean dfs(int node, List<List<Integer>> adj, boolean[] vis, boolean[] path) {
+        vis[node] = path[node] = true;
+
+        for (int next : adj.get(node))
+            if (!vis[next] && dfs(next, adj, vis, path)) return true;
+            else if (path[next]) return true;
+            
+        path[node] = false;
         return false;
     }
 }
