7e2eff91a1e89cd64c151651dea60e107a4d9eab
Anupam <146722854+anupamchaubey@users.noreply.github.com>
2026-09-25 18:54:20 +0530

    Time: 126 ms (9.51%), Space: 47 MB (5.25%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -7,17 +7,16 @@ class Solution {
     }
 
     int rec(int[] nums, int st, int en) {
-        if (en < 0 || st == nums.length || st > en)
+        if (st > en)
             return 0;
         if (dp[st][en] != null)
             return dp[st][en];
         int max = 0;
         for (int i = st; i <= en; i++) {
-            int l = rec(nums, st, i - 1);
-            int r = rec(nums, i + 1, en);
-            int cost = nums[i] * ((st - 1 >= 0) ? nums[st - 1] : 1)
-                    * ((en + 1 < nums.length) ? nums[en + 1] : 1);
-            max = Math.max(max, cost + l + r);
+            int left = rec(nums, st, i - 1);
+            int right = rec(nums, i + 1, en);
+            int val = ((st - 1 >= 0) ? nums[st - 1] : 1) * ((en + 1 < nums.length) ? nums[en + 1] : 1) * nums[i];
+            max = Math.max(max, val + left + right);
         }
         return dp[st][en] = max;
     }
